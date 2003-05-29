d0af3c8421c077daf9b7470846d9861cf5c9daed
kgarner <kgarner@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-05-29 21:49:41 +0000

    Enum needed for table class

---
+++ variman/trunk/project/hibernate/java/enums/org/realtors/rets/server/metadata/AlignmentEnum.java
@@ -0,0 +1,32 @@
+package org.nar.rets.metadata.newdb;
+
+import net.sf.hibernate.PersistentEnum;
+
+public class AlignmentEnum implements PersistentEnum
+{
+    public static final AlignmentEnum LEFT = new AlignmentEnum(0);
+    public static final AlignmentEnum RIGHT = new AlignmentEnum(1);
+    public static final AlignmentEnum CENTER = new AlignmentEnum(2);
+    public static final AlignmentEnum JUSTIFY = new AlignmentEnum(3);
+
+    private AlignmentEnum(int code)
+    {
+        this.mCode = code;
+    }
+
+    public int toInt() { return mCode; }
+
+    public static AlignmentEnum fromInt(int code)
+    {
+        switch (code)
+        {
+            case 0: return LEFT;
+            case 1: return RIGHT;
+            case 2: return CENTER;
+            case 3: return JUSTIFY;
+            default: throw new RuntimeException("Unknown Alignment");
+        }
+    }
+
+    private final int mCode;
+}
---
+++ variman/trunk/project/hibernate/java/enums/org/realtors/rets/server/metadata/DataTypeEnum.java
@@ -0,0 +1,44 @@
+package org.nar.rets.metadata.newdb;
+
+import net.sf.hibernate.PersistentEnum;
+
+public class DataTypeEnum implements PersistentEnum
+{
+    public static final DataTypeEnum BOOLEAN = new DataTypeEnum(0);
+    public static final DataTypeEnum CHARACTER = new DataTypeEnum(1);
+    public static final DataTypeEnum DATE = new DataTypeEnum(2);
+    public static final DataTypeEnum DATETIME = new DataTypeEnum(3);
+    public static final DataTypeEnum TIME = new DataTypeEnum(4);
+    public static final DataTypeEnum TINY = new DataTypeEnum(5);
+    public static final DataTypeEnum SMALL = new DataTypeEnum(6);
+    public static final DataTypeEnum INT = new DataTypeEnum(7);
+    public static final DataTypeEnum LONG = new DataTypeEnum(8);
+    public static final DataTypeEnum DECIMAL = new DataTypeEnum(9);
+
+    private DataTypeEnum(int dataType)
+    {
+        this.mDataType = dataType;
+    }
+
+    public int toInt() { return mDataType; }
+
+    public static DataTypeEnum fromInt(int dataType)
+    {
+        switch (dataType)
+        {
+            case 0: return BOOLEAN;
+            case 1: return CHARACTER;
+            case 2: return DATE;
+            case 3: return DATETIME;
+            case 4: return TIME;
+            case 5: return TINY;
+            case 6: return SMALL;
+            case 7: return INT;
+            case 8: return LONG;
+            case 9: return DECIMAL;
+            default: throw new RuntimeException("Unknown DataType");
+        }
+    }
+
+    private final int mDataType;
+}
