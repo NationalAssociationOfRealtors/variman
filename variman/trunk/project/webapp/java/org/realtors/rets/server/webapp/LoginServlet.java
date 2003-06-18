8bc3e41f970d830919809f7b14437c81fad0395d
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-06-18 17:33:17 +0000

    Add hard coded RETS response.

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/LoginServlet.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/LoginServlet.java
@@ -1,12 +1,12 @@
 package org.realtors.rets.server.webapp;
 
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 
 /**
  * Performs all necessary one-time initializations for the web
@@ -19,20 +19,26 @@ public class LoginServlet extends HttpServlet
 {
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
-        throws ServletException, IOException
+            throws ServletException, IOException
     {
-        HttpSession session = request.getSession();
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>RETS Login Servlet</title>");
-        out.println("</head>");
-        out.println();
-        out.println("<body>");
-        out.println("<h1 align='center'>RETS Login Servlet</h1>");
-        out.println("<p>This is the RETS login servlet.</p>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println(
+                "<RETS ReplyCode=\"0\" ReplyText=\"Operation Successful\">");
+        out.println("<RETS-RESPONSE>");
+        out.println("Broker = B123, BO987");
+        out.println("MemberName = Joe T. Schmoe");
+        out.println("MetadataVersion = 1.0.000");
+        out.println("MinMetadataVersion = 1.00.000");
+        out.println("User = A123,5678,1,A123");
+        out.println("Login = http://rets.test:6103/login");
+        out.println("Logout = http://rets.test:6103/logout");
+        out.println("Search = http://rets.test:6103/search");
+        out.println("GetMetadata = http://rets.test:6103/getMetadata");
+        out.println("ChangePassword = http://rets.test:6103/changePassword");
+        out.println("GetObject = http://rets.test:6103/get");
+        out.println("Balance = 44.21");
+        out.println("TimeoutSeconds = 60");
+        out.println("</RETS-RESPONSE></RETS>");
     }
 }
