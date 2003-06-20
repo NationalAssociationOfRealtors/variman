697ba419f72cac55cabe399d3377cc79e53420d5
kgarner <kgarner@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-06-20 21:51:22 +0000

    System and start of Resource

--- variman/trunk/project/importer/java/org/realtors/rets/server/importer/MetadataImporter.java
+++ variman/trunk/project/importer/java/org/realtors/rets/server/importer/MetadataImporter.java
@@ -1,15 +1,21 @@
 package org.realtors.rets.server.importer;
 
-import java.util.Enumeration;
+import java.util.List;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.HashSet;
 
 import net.sf.hibernate.Hibernate;
 import net.sf.hibernate.cfg.Configuration;
+import net.sf.hibernate.Session;
 import net.sf.hibernate.SessionFactory;
 import net.sf.hibernate.MappingException;
 import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Transaction;
 
 import org.realtors.rets.client.RetsSession;
 import org.realtors.rets.client.MetadataTable;
+import org.realtors.rets.client.Metadata;
 
 import org.realtors.rets.server.data.RETSData;
 import org.realtors.rets.server.data.RETSDataElement;
@@ -87,16 +93,61 @@ public class MetadataImporter
         RetsSession session =
             new RetsSession("http://demo.crt.realtors.org:6103/login");
         session.login("Joe", "Schmoe");
-        MetadataTable mt = session.getSystemMetadata();
+        parseMetadata(session);
         session.logout();
-        parseMetadata(mt);
     }
 
-    private void parseMetadata(MetadataTable mt)
+    private void parseMetadata(RetsSession rSession)
+        throws Exception
     {
-        System.out.println(mt);
+        Session hSession = mSessions.openSession();
+        Transaction tx = hSession.beginTransaction();
+
+        MSystem hSystem = system(rSession, hSession);
+        Set hResources = resource(rSession, hSession, hSystem);
+        
+        tx.commit();
+        hSession.close();
     }
-       
+
+    private MSystem system(RetsSession rSession, Session hSession)
+        throws HibernateException
+    {
+        MetadataTable tSystem =
+            rSession.getMetadataTable(MetadataTable.SYSTEM);
+        MSystem hSystem = new MSystem();
+
+        hSession.save(hSystem);
+        return hSystem;
+    }
+
+    private Set resource(RetsSession rSession, Session hSession,
+                         MSystem hSystem)
+        throws HibernateException
+    {
+        MetadataTable tResource =
+            rSession.getMetadataTable(MetadataTable.RESOURCE);
+
+        Set hResources = new HashSet();
+        List resources = tResource.getDataRows("");
+        Iterator i = resources.iterator();
+        while (i.hasNext())
+        {
+            Metadata md = (Metadata) i.next();
+            Resource hResource = new Resource();
+
+            hResource.setSystemid(hSystem);
+            // do more stuff
+
+            hSession.save(hResource);
+            hResources.add(hResource);
+        }
+
+        hSystem.setResources(hResources);
+
+        // change this
+        return null;
+    }
 
     public static final void main(String args[])
         throws Exception
@@ -108,5 +159,5 @@ public class MetadataImporter
     private SessionFactory mSessions;
     
     private static final String CVSID =
-        "$Id: MetadataImporter.java,v 1.2 2003/06/19 21:54:06 kgarner Exp $";
+        "$Id: MetadataImporter.java,v 1.3 2003/06/20 21:51:22 kgarner Exp $";
 }
