be13d0e0dab6f14781bc2fe85efd2f39db03e8d4
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-06-25 17:09:15 +0000

    Optimize imports.

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/GetMetadataServlet.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/GetMetadataServlet.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 
 public class GetMetadataServlet extends RetsServlet
 {
