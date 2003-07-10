4a4631718ab7477ec07bbecc0313b0b0906ee192
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-07-10 17:13:54 +0000

    Complete table formatter

--- variman/trunk/project/hibernate/test/java/org/realtors/rets/server/metadata/format/TableFormatterTest.java
+++ variman/trunk/project/hibernate/test/java/org/realtors/rets/server/metadata/format/TableFormatterTest.java
@@ -2,11 +2,17 @@
  */
 package org.realtors.rets.server.metadata.format;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.realtors.rets.server.metadata.Table;
 import org.realtors.rets.server.metadata.TableStandardName;
 import org.realtors.rets.server.metadata.DataTypeEnum;
 import org.realtors.rets.server.metadata.InterpretationEnum;
 import org.realtors.rets.server.metadata.AlignmentEnum;
+import org.realtors.rets.server.metadata.Lookup;
+import org.realtors.rets.server.metadata.UnitEnum;
+import org.realtors.rets.server.metadata.EditMask;
 
 public class TableFormatterTest extends FormatterTestCase
 {
@@ -17,20 +23,34 @@ public class TableFormatterTest extends FormatterTestCase
         table.setStandardName(new TableStandardName("ElementarySchool"));
         table.setLongName("Elementary School");
         table.setShortName("ElemSchool");
+        table.setDbName("E_SCHOOL");
         table.setMaximumLength(4);
         table.setDataType(DataTypeEnum.INT);
-        table.setPrecision(1);
+        table.setPrecision(0);
         table.setSearchable(true);
         table.setInterpretation(InterpretationEnum.LOOKUP);
         table.setAlignment(AlignmentEnum.LEFT);
+
+        EditMask em1 = new EditMask(1);
+        em1.setEditMaskID("EM1");
+        EditMask em2 = new EditMask(2);
+        em2.setEditMaskID("EM2");
+        Set editMasks = new HashSet();
+        editMasks.add(em1);
+        editMasks.add(em2);
+        table.setEditMasks(editMasks);
+
         table.setUseSeparator(false);
-//        table.setLookup(null);
-        table.setMaxSelect(2);
-        table.setIndex(3);
-        table.setMinimum(4);
-        table.setMaximum(5);
-        table.setDefault(6);
-        table.setRequired(7);
+        Lookup lookup = new Lookup();
+        lookup.setLookupName("E_SCHOOL");
+        table.setLookup(lookup);
+        table.setMaxSelect(1);
+        table.setUnits(UnitEnum.FEET);
+        table.setIndex(2);
+        table.setMinimum(3);
+        table.setMaximum(4);
+        table.setDefault(5);
+        table.setRequired(6);
         table.setUnique(false);
         mTables = new Table[] {table};
     }
@@ -53,8 +73,8 @@ public class TableFormatterTest extends FormatterTestCase
 
             "<DATA>\t" +
             "E_SCHOOL\tElementarySchool\tElementary School\t" +
-//            "E_SCHOOL\tElemnSchool\t4\tInt\t0\t1\tLookup\tLeft\t0\t\t\t" +
-//            "0\t\t0\t0\t0\t0\t0\t0\t" +
+            "E_SCHOOL\tElemSchool\t4\tInt\t0\t1\tLookup\tLeft\t0\tEM1,EM2\t" +
+            "E_SCHOOL\t1\tFeet\t2\t3\t4\t5\t6\t\t0\t" +
             "</DATA>\n" +
 
             "</METADATA-TABLE>\n",
