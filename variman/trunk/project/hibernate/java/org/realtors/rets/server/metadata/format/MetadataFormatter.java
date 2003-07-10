b02a27ee4b89e047f828b50cfaf7a10b7aec6f0e
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-07-10 21:37:25 +0000

    TagBuilder removes need for format(Date)

--- variman/trunk/project/hibernate/java/org/realtors/rets/server/metadata/format/MetadataFormatter.java
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/metadata/format/MetadataFormatter.java
@@ -2,10 +2,7 @@
  */
 package org.realtors.rets.server.metadata.format;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
 public class MetadataFormatter
 {
@@ -19,14 +16,6 @@ public class MetadataFormatter
     public static int getDefaultFormat()
     {
         return sDefaultFormat;
-    }
-
-    protected String format(Date date)
-    {
-        DateFormat formatter =
-            new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
-        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-        return formatter.format(date);
     }
 
     public void setVersion(String version, Date date)
