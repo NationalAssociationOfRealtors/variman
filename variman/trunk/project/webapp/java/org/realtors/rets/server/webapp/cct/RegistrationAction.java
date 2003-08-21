66b6d8b92d9cf1a2e79f9569cb2d10c60aa3455b
kgarner <kgarner@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-21 21:10:32 +0000

    stub for now

---
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/RegistrationAction.java
@@ -0,0 +1,38 @@
+/*
+ * Created on Aug 21, 2003
+ *
+ */
+package org.realtors.rets.server.webapp.cct;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author kgarner
+ */
+public class RegistrationAction extends Action
+{
+
+    /* (non-Javadoc)
+     * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm actionForm,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception
+    {
+        if (isCancelled(request))
+        {
+            return new ActionForward(mapping.getInput());
+        }
+
+        return mapping.findForward("userPage");
+    }
+
+}
