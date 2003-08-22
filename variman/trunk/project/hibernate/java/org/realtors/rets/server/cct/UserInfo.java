7bcbef1677a04b4376f0dd3902d39efc39d650c0
kgarner <kgarner@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-22 19:42:35 +0000

    First of needed hibernate objects for test frontend

---
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/cct/UserInfo.java
@@ -0,0 +1,160 @@
+/*
+ * Created on Aug 22, 2003
+ *
+ */
+package org.realtors.rets.server.cct;
+
+import org.realtors.rets.server.User;
+
+/**
+ * @hibernate.class table="rets_cct_userinfo"
+ *                  proxy="org.realtors.rets.server.cct.UserInfo"
+ */
+public class UserInfo
+{
+    /**
+     * @hibernate.property length="80" not-null="true"
+     */
+    public String getAgentID()
+    {
+        return mAgentID;
+    }
+
+    /**
+     * @hibernate.property length="80" not-null="true" 
+     */
+    public String getCompany()
+    {
+        return mCompany;
+    }
+
+    /**
+     * @hibernate.property length="80" not-null="true"
+     */
+    public String getEmail()
+    {
+        return mEmail;
+    }
+
+    /**
+     * @hibernate.id generator-class="native"
+     */
+    public Long getId()
+    {
+        return mId;
+    }
+
+    /**
+     * @hibernate.property length="80" not-null="true" 
+     */
+    public String getProductName()
+    {
+        return mProductName;
+    }
+
+    /**
+     * @hibernate.property length="80" not-null="true" 
+     */
+    public String getProductVersion()
+    {
+        return mProductVersion;
+    }
+
+    /**
+     * @hibernate.many-to-one
+     * @hibernate.column name="user_id" not-null="true"
+     */
+    public User getUser()
+    {
+        return mUser;
+    }
+
+    /**
+     * @hibernate.property length="80" not-null="true"
+     */
+    public String getUserAgent()
+    {
+        return mUserAgent;
+    }
+
+    /**
+     * 
+     * @param string
+     */
+    public void setAgentID(String string)
+    {
+        mAgentID = string;
+    }
+
+    /**
+     * 
+     * @param string
+     */
+    public void setCompany(String string)
+    {
+        mCompany = string;
+    }
+
+    /**
+     * 
+     * @param string
+     */
+    public void setEmail(String string)
+    {
+        mEmail = string;
+    }
+
+    /**
+     * 
+     * @param long1
+     */
+    public void setId(Long long1)
+    {
+        mId = long1;
+    }
+
+    /**
+     * 
+     * @param string
+     */
+    public void setProductName(String string)
+    {
+        mProductName = string;
+    }
+
+    /**
+     * 
+     * @param string
+     */
+    public void setProductVersion(String string)
+    {
+        mProductVersion = string;
+    }
+
+    /**
+     * 
+     * @param user
+     */
+    public void setUser(User user)
+    {
+        mUser = user;
+    }
+
+    /**
+     * 
+     * @param string
+     */
+    public void setUserAgent(String string)
+    {
+        mUserAgent = string;
+    }
+
+    private String mAgentID;
+    private String mCompany;
+    private String mEmail;
+    private Long mId;
+    private String mProductName;
+    private String mProductVersion;
+    private User mUser;
+    private String mUserAgent;
+}
