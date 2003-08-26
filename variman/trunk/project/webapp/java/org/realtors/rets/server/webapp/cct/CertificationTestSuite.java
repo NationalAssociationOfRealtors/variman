90a58a9d514bef9c66a0705e9a77174be885a900
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-26 20:26:13 +0000

    Add test suite

---
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/CertificationTestSuite.java
@@ -0,0 +1,38 @@
+/*
+ */
+package org.realtors.rets.server.webapp.cct;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+import org.realtors.rets.server.webapp.cct.tests.NormalLogin;
+import org.realtors.rets.server.webapp.cct.tests.RelativeUrlLogin;
+
+public class CertificationTestSuite
+{
+    public CertificationTestSuite(String testContext)
+    {
+        mTests = new ArrayList();
+        mTests.add(new NormalLogin());
+        mTests.add(new RelativeUrlLogin());
+
+        for (int i = 0; i < mTests.size(); i++)
+        {
+            CertificationTest test = (CertificationTest) mTests.get(i);
+            test.init(testContext);
+        }
+    }
+
+    public Iterator getTests()
+    {
+        return mTests.iterator();
+    }
+
+    public CertificationTest getTest(int index)
+    {
+        return (CertificationTest) mTests.get(index);
+    }
+
+    private List mTests;
+}
