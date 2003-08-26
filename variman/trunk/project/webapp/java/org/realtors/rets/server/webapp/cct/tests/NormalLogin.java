38fa8f42d9b7c90459ab436c1d26d92b1f093146
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-26 19:06:40 +0000

    Add first simple test

---
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/tests/NormalLogin.java
@@ -0,0 +1,57 @@
+/*
+ */
+package org.realtors.rets.server.webapp.cct.tests;
+
+import org.realtors.rets.server.webapp.cct.BaseCertificationTest;
+import org.realtors.rets.server.webapp.cct.ValidationResults;
+import org.realtors.rets.server.webapp.cct.HandlerManager;
+import org.realtors.rets.server.webapp.cct.LoginHandler;
+import org.realtors.rets.server.webapp.cct.InvokeCount;
+import org.realtors.rets.server.webapp.cct.ActionHandler;
+import org.realtors.rets.server.webapp.cct.LogoutHandler;
+
+public class NormalLogin extends BaseCertificationTest
+{
+
+    public String getProcedure()
+    {
+        return "Login, then logout.";
+    }
+
+    protected ValidationResults validate()
+    {
+        ValidationResults results = new ValidationResults();
+        login.validate(results);
+        action.validate(results);
+        logout.validate(results);
+        return results;
+    }
+
+    public String getDescription()
+    {
+        return "Tests a normal login";
+    }
+
+    public void start()
+    {
+        HandlerManager actionManager = HandlerManager.getInstance();
+        login = actionManager.getLoginHandler(mTestContext);
+        login.setSessionId("login-headers");
+        login.setGetInvokeCount(InvokeCount.ONE);
+        login.addRequiredHeader("Accept", "^\\*/\\*$");
+        login.addRequiredHeader("User-Agent", "^[^/]+(/[^/])?$");
+        login.addRequiredHeader("RETS-Version", ".*");
+
+        action = actionManager.getActionHandler();
+        action.setGetInvokeCount(InvokeCount.ONE);
+        action.addCookie("RETS-Session-ID", "login-headers");
+
+        logout = actionManager.getLogoutHandler();
+        logout.setGetInvokeCount(InvokeCount.ZERO_OR_ONE);
+//        logout.addCookie("RETS-Session-ID", "login-headers");
+    }
+
+    private LoginHandler login;
+    private ActionHandler action;
+    private LogoutHandler logout;
+}
