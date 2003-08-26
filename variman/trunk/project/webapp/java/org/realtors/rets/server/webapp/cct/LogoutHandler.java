75b470425ef548131d541e59b5b09dd004a07e22
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-26 19:04:43 +0000

    Extend from base handler

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/LogoutHandler.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/LogoutHandler.java
@@ -8,7 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LogoutHandler implements ServletHandler
+public class LogoutHandler extends BaseServletHandler
 {
     public static final String NAME = "/logout";
 
