e60d74b736f88ec4f537e72ecb38573f84674994
dterrell <dterrell@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-27 16:04:14 +0000

    compress RetsAction into CctAction

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/CctAction.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/CctAction.java
@@ -1,11 +1,12 @@
 package org.realtors.rets.server.webapp.cct;
 
-import java.util.List;
-import java.util.ArrayList;
 import javax.servlet.http.HttpSession;
 
-import org.realtors.rets.server.webapp.cct.tests.NormalLogin;
+import org.apache.struts.action.Action;
+
 import org.realtors.rets.server.User;
+import org.realtors.rets.server.webapp.auth.AuthenticationFilter;
+import org.realtors.rets.server.cct.UserInfo;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,17 +15,45 @@ import org.realtors.rets.server.User;
  * Time: 2:05:42 PM
  * To change this template use Options | File Templates.
  */
-public abstract class CctAction extends RetsAction
+public abstract class CctAction extends Action
 {
-    void loadTestSuite(HttpSession session)
+    void loadCctState(HttpSession session)
     {
-        if (session.getAttribute("suite") == null)
+        User user = (User) session.getAttribute(USER_KEY);
+        UserInfo info = (UserInfo) session.getAttribute(USERINFO_KEY);
+        CertificationTestSuite suite =
+            (CertificationTestSuite) session.getAttribute(TESTSUITE_KEY);
+        if (user == null)
         {
-            User user = getUser(session);
-
-            CertificationTestSuite suite = new CertificationTestSuite(
-                user.getUsername());
-            session.setAttribute("suite", suite);
+            user = getUser(session);
+            session.setAttribute(USER_KEY, user);
+        }
+        if (info == null)
+        {
+            info = getUserInfo(session);
+            session.setAttribute(USERINFO_KEY, info);
+        }
+        if (suite == null)
+        {
+            suite = new CertificationTestSuite(user.getUsername());
+            session.setAttribute(TESTSUITE_KEY, suite);
         }
     }
+
+    protected User getUser(HttpSession session)
+    {
+        return (User) session.getAttribute(
+            AuthenticationFilter.AUTHORIZED_USER_KEY);
+    }
+
+    protected UserInfo getUserInfo(HttpSession session)
+    {
+        User user = getUser(session);
+        return UTILS.getUserInfo(user.getUsername());
+    }
+
+    public static final String USER_KEY = "user";
+    public static final String USERINFO_KEY = "userInfo";
+    public static final String TESTSUITE_KEY = "suite";
+    private static final UserUtils UTILS = new UserUtils();
 }
