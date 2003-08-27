9e30d662440787e7a25d8684ffef527c36c63218
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-27 21:22:15 +0000

    Add start/stop actions

---
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/StartTestAction.java
@@ -0,0 +1,34 @@
+package org.realtors.rets.server.webapp.cct;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionForm;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
+
+public class StartTestAction extends CctAction
+{
+
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm actionForm,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception
+    {
+        loadCctState(request.getSession());
+        TestForm form = (TestForm) actionForm;
+        CertificationTestSuite suite = getSuite(request.getSession());
+        CertificationTest test = suite.getTest(form.getTestNumber());
+        LOG.debug("Starting test #" + form.getTestNumber() + ": " +
+                  test.getDescription());
+        test.start();
+        return mapping.findForward("home");
+    }
+
+    private static final Logger LOG =
+        Logger.getLogger(StartTestAction.class);
+}
