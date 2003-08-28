61666ef69b520488ad846bae6260adbbb07e6ea8
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-28 17:26:31 +0000

    Move from webapp package

---
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/RetsReplyException.java
@@ -0,0 +1,32 @@
+/*
+ */
+package org.realtors.rets.server;
+
+import org.apache.commons.lang.exception.NestableException;
+
+/**
+ * A class representing an error case that can be represented by a RETS reply
+ * code.
+ */
+public class RetsReplyException extends NestableException
+{
+    public RetsReplyException(int replyCode, String meaning)
+    {
+        super(meaning);
+        mReplyCode = replyCode;
+        mMeaning = meaning;
+    }
+
+    public int getReplyCode()
+    {
+        return mReplyCode;
+    }
+
+    public String getMeaning()
+    {
+        return mMeaning;
+    }
+
+    private int mReplyCode;
+    private String mMeaning;
+}
