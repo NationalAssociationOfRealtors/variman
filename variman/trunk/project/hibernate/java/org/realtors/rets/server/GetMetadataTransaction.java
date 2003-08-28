0ea4407622286dff39c29ce96e3faebc743f12b1
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-28 17:28:14 +0000

    Move bulk of GetMetadataServlet into GetMetadataTransaction

---
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/GetMetadataTransaction.java
@@ -0,0 +1,58 @@
+/*
+ */
+package org.realtors.rets.server;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import org.realtors.rets.server.metadata.MetadataSegment;
+import org.realtors.rets.server.metadata.format.MetadataSegmentFormatter;
+
+import org.apache.log4j.Logger;
+
+public class GetMetadataTransaction
+{
+    /**
+     * Gets metadata and formats it. Only throws an exception if an error
+     * occured while getting metadata. This guarantees the print writer is
+     * clear from output if an error occurs.
+     *
+     * @param out Formats output to this writer.
+     * @param parameters Get metadata parameters.
+     * @param fetcher Used to get the metadata.
+     * @throws RetsReplyException If an error occured while getting the
+     * metadata.
+     */
+    public void execute(PrintWriter out, GetMetadataParameters parameters,
+                        MetadataFetcher fetcher)
+        throws RetsReplyException
+    {
+        List metadataObjects = fetcher.fetchMetadata(parameters.getType(),
+                                                     parameters.getIds(),
+                                                     parameters.isRecursive());
+
+        out.println("<RETS ReplyCode=\"0\" " +
+                    "ReplyText=\"Operation Successful\">");
+        formatOutput(out, metadataObjects, parameters.getFormat());
+        out.println("</RETS>");
+    }
+
+    public void formatOutput(PrintWriter out, List metadataSegments,
+                              int format)
+    {
+        LOG.debug("Formatting " + metadataSegments.size() + " segments");
+        long start = System.currentTimeMillis();
+        MetadataSegmentFormatter formatter =
+            new MetadataSegmentFormatter(out, format);
+        for (int i = 0; i < metadataSegments.size(); i++)
+        {
+            MetadataSegment metadataSegment =
+                (MetadataSegment) metadataSegments.get(i);
+            formatter.format(metadataSegment);
+        }
+        LOG.debug("Formatting done: " + (System.currentTimeMillis() - start));
+    }
+
+    private static final Logger LOG =
+        Logger.getLogger(GetMetadataTransaction.class);
+}
