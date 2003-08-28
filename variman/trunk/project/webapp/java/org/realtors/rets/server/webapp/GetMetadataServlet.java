7e5f7370f4af0507c5064af9f8e745f22a6972ce
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-28 17:29:27 +0000

    Move bulk of GetMetadataServlet to GetMetadataTransaction

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/GetMetadataServlet.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/GetMetadataServlet.java
@@ -4,47 +4,19 @@ package org.realtors.rets.server.webapp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.hibernate.HibernateException;
-import net.sf.hibernate.Session;
-
-import org.realtors.rets.server.SessionHelper;
-import org.realtors.rets.server.metadata.EditMask;
-import org.realtors.rets.server.metadata.Lookup;
-import org.realtors.rets.server.metadata.LookupType;
-import org.realtors.rets.server.metadata.MClass;
-import org.realtors.rets.server.metadata.MObject;
-import org.realtors.rets.server.metadata.MSystem;
+import org.realtors.rets.server.GetMetadataParameters;
+import org.realtors.rets.server.GetMetadataTransaction;
+import org.realtors.rets.server.HibernateMetadataFetcher;
+import org.realtors.rets.server.ManagerMetadataFetcher;
+import org.realtors.rets.server.MetadataFetcher;
+import org.realtors.rets.server.RetsReplyException;
 import org.realtors.rets.server.metadata.MetadataManager;
-import org.realtors.rets.server.metadata.MetadataSegment;
-import org.realtors.rets.server.metadata.Resource;
-import org.realtors.rets.server.metadata.SearchHelp;
-import org.realtors.rets.server.metadata.ServerMetadata;
-import org.realtors.rets.server.metadata.Table;
-import org.realtors.rets.server.metadata.Update;
-import org.realtors.rets.server.metadata.UpdateType;
-import org.realtors.rets.server.metadata.ValidationExpression;
-import org.realtors.rets.server.metadata.ValidationExternal;
-import org.realtors.rets.server.metadata.ValidationExternalType;
-import org.realtors.rets.server.metadata.ValidationLookup;
-import org.realtors.rets.server.metadata.ValidationLookupType;
-import org.realtors.rets.server.metadata.format.MetadataFormatter;
-import org.realtors.rets.server.metadata.format.MetadataSegmentFormatter;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -55,24 +27,16 @@ public class  GetMetadataServlet extends RetsServlet
 {
     public void init() throws ServletException
     {
-        mUseCache = true;
-        mValidTypes = new HashSet();
-        mValidTypes.add(MSystem.TABLE);
-        mValidTypes.add(Resource.TABLE);
-        mValidTypes.add(MClass.TABLE);
-        mValidTypes.add(Table.TABLE);
-        mValidTypes.add(Update.TABLE);
-        mValidTypes.add(UpdateType.TABLE);
-        mValidTypes.add(MObject.TABLE);
-        mValidTypes.add(SearchHelp.TABLE);
-        mValidTypes.add(EditMask.TABLE);
-        mValidTypes.add(Lookup.TABLE);
-        mValidTypes.add(LookupType.TABLE);
-        mValidTypes.add(ValidationLookup.TABLE);
-        mValidTypes.add(ValidationLookupType.TABLE);
-        mValidTypes.add(ValidationExternal.TABLE);
-        mValidTypes.add(ValidationExternalType.TABLE);
-        mValidTypes.add(ValidationExpression.TABLE);
+        mTransaction = new GetMetadataTransaction();
+        if (USE_CACHE)
+        {
+            mMetadataFetcher = new ServletMetadataFetcher();
+        }
+        else
+        {
+            mMetadataFetcher =
+                new HibernateMetadataFetcher(InitServlet.getSessions());
+        }
     }
 
     protected void doGet(HttpServletRequest request,
@@ -84,38 +48,9 @@ public class  GetMetadataServlet extends RetsServlet
 
         try
         {
-            String type = request.getParameter("Type");
-            String id = request.getParameter("ID");
-            String formatString = request.getParameter("Format");
-            LOG.debug("type=" + type + ", id=" + id + ", format=" +
-                      formatString);
-
-            type = cleanUpType(type);
-            int format = parseFormat(formatString);
-            String[] ids = StringUtils.split(id, ":");
-
-            // Clean up id
-            boolean recursive = false;
-            String lastId = ids[ids.length - 1];
-            if (lastId.equals("*"))
-            {
-                recursive = true;
-                // chop off "*"
-                ids = shrinkByOne(ids);
-            }
-            else if (lastId.equals("0"))
-            {
-                recursive = false;
-                // chop off "0"
-                ids = shrinkByOne(ids);
-            }
-
-            List metadataObjects = getMetadata(type, ids, recursive);
-
-            out.println("<RETS ReplyCode=\"0\" " +
-                        "ReplyText=\"Operation Successful\">");
-            formatOutput(out, metadataObjects, format);
-            out.println("</RETS>");
+            GetMetadataParameters parameters =
+                new GetMetadataParameters(request.getParameterMap());
+            mTransaction.execute(out, parameters, mMetadataFetcher);
         }
         catch(RetsReplyException e)
         {
@@ -130,224 +65,20 @@ public class  GetMetadataServlet extends RetsServlet
         }
     }
 
-    private String cleanUpType(String type) throws RetsReplyException
+    /**
+     * Gets the metadata manager from the servlet request.
+     */
+    class ServletMetadataFetcher extends ManagerMetadataFetcher
     {
-        type = type.toUpperCase();
-        if (!type.startsWith("METADATA-"))
+        protected MetadataManager getMetadataManager()
         {
-            throw new RetsReplyException(20501, "Invalid Type");
+            return GetMetadataServlet.this.getMetadataManager();
         }
-
-        type = type.substring("METADATA-".length());
-        if (!mValidTypes.contains(type))
-        {
-            throw new RetsReplyException(20501, "Invalid Type");
-        }
-        return type;
-    }
-
-    private int parseFormat(String formatString)
-    {
-        formatString = formatString.toUpperCase();
-        if ((formatString == null) || formatString.equals(COMPACT_FORMAT))
-        {
-            return MetadataFormatter.COMPACT;
-        }
-        else
-        {
-            throw new IllegalArgumentException("Unknown formatString: " +
-                                               formatString);
-        }
-    }
-
-    private static String[] shrinkByOne(String[] array)
-    {
-        String[] newArray = new String[array.length - 1];
-        System.arraycopy(array, 0, newArray, 0, array.length - 1);
-        return newArray;
-    }
-
-    private void formatOutput(PrintWriter out, List metadataSegments,
-                              int format)
-    {
-        LOG.debug("Formatting " + metadataSegments.size() + " segments");
-        long start = System.currentTimeMillis();
-        MetadataSegmentFormatter formatter =
-            new MetadataSegmentFormatter(out, format);
-        for (int i = 0; i < metadataSegments.size(); i++)
-        {
-            MetadataSegment metadataSegment =
-                (MetadataSegment) metadataSegments.get(i);
-            formatter.format(metadataSegment);
-        }
-        LOG.debug("Formatting done: " + (System.currentTimeMillis() - start));
-    }
-
-    private List getMetadata(String type, String[] levels, boolean recursive)
-        throws RetsReplyException
-    {
-        if (mUseCache)
-        {
-            return getMetadataFromCache(type, levels, recursive);
-        }
-        else
-        {
-            return getMetadataFromHibernate(type, levels, recursive);
-        }
-    }
-
-    private List getMetadataFromCache(String type, String[] levels,
-                                      boolean recursive)
-    {
-        // Always need system to get version and date
-        MSystem system = findSystem();
-        String version = system.getVersionString();
-        Date date = system.getDate();
-        List metadataResults = new ArrayList();
-
-        MetadataManager manager = getMetadataManager();
-        List metadata = manager.find(type, StringUtils.join(levels, ":"));
-        metadataResults.add(new MetadataSegment(metadata, levels, version,
-                                                date));
-        if (recursive)
-        {
-            recurseChildren(metadata,  metadataResults, version, date);
-        }
-        return metadataResults;
-    }
-
-    private List getMetadataFromHibernate(String type, String[] levels,
-                                          boolean recursive)
-        throws RetsReplyException
-    {
-        // Always need system to get version and date
-        MSystem system = findSystemFromHibernate();
-        String version = system.getVersionString();
-        Date date = system.getDate();
-        List metadataResults = new ArrayList();
-
-        MetadataFinder finder =
-            (MetadataFinder) sMetadataFinders.get(type);
-        if (finder != null)
-        {
-            LOG.debug("Using finder for type: " + type);
-            long start = System.currentTimeMillis();
-            List metadata = finder.findMetadata(levels);
-            LOG.debug("End finder: " + (System.currentTimeMillis() - start));
-            metadataResults.add(new MetadataSegment(metadata, levels, version,
-                                                    date));
-            if (recursive)
-            {
-                recurseChildren(metadata,  metadataResults, version, date);
-            }
-        }
-        else
-        {
-            LOG.warn("Recieved query for unknown metadataResults type: " +
-                     type + ", level=" + StringUtils.join(levels, ":"));
-            throw new RetsReplyException(20501, "Invalid Type");
-        }
-        return metadataResults;
-    }
-
-
-    private void recurseChildren(List parents, List metadataResults,
-                                 String version, Date date)
-    {
-        for (int i = 0; i < parents.size(); i++)
-        {
-            ServerMetadata metadata = (ServerMetadata) parents.get(i);
-            List children = metadata.getChildren();
-            String[] childLevels = metadata.getPathAsArray();
-            for (int j = 0; j < children.size(); j++)
-            {
-                ServerMetadata[] child = (ServerMetadata[]) children.get(j);
-                metadataResults.add(new MetadataSegment(child, childLevels,
-                                                        version, date));
-                recurseChildren(Arrays.asList(child), metadataResults,
-                                version, date);
-            }
-        }
-    }
-
-    private MSystem findSystem()
-    {
-        MetadataManager manager = getMetadataManager();
-        List systems = manager.find(MSystem.TABLE, "");
-        return (MSystem) systems.get(0);
-    }
-
-    private MSystem findSystemFromHibernate()
-    {
-        SessionHelper helper = InitServlet.createHelper();
-        MSystem system = null;
-        try
-        {
-            Session session = helper.beginTransaction();
-            List results = new ArrayList();
-            Iterator iterator = session.iterate("from MSystem");
-            while (iterator.hasNext())
-            {
-                results.add(iterator.next());
-            }
-            if (results.size() == 1)
-            {
-                system = (MSystem) results.get(0);
-            }
-        }
-        catch (HibernateException e)
-        {
-            LOG.warn("Caught", e);
-            helper.rollback(LOG);
-        }
-        finally
-        {
-            helper.close(LOG);
-        }
-        return system;
     }
 
     private static final Logger LOG =
         Logger.getLogger(GetMetadataServlet.class);
-    public static final String COMPACT_FORMAT = "COMPACT";
-    private static Map sMetadataFinders;
-    private Set mValidTypes;
-    private boolean mUseCache;
-
-    static
-    {
-        sMetadataFinders = new HashMap();
-        sMetadataFinders.put("SYSTEM",
-                              new MetadataFinder("MSystem", 0));
-        sMetadataFinders.put("RESOURCE",
-                              new MetadataFinder("Resource", 0));
-        sMetadataFinders.put("CLASS",
-                              new MetadataFinder("MClass", 1));
-        sMetadataFinders.put("TABLE",
-                             new MetadataFinder("Table", 2));
-        sMetadataFinders.put("UPDATE",
-                             new MetadataFinder("Update", 2));
-        sMetadataFinders.put("UPDATE_TYPE",
-                             new MetadataFinder("UpdateType", 3));
-        sMetadataFinders.put("OBJECT",
-                             new MetadataFinder("MObject", 1));
-        sMetadataFinders.put("SEARCH_HELP",
-                             new MetadataFinder("SearchHelp", 1));
-        sMetadataFinders.put("EDIT_MASK",
-                             new MetadataFinder("EditMask", 1));
-        sMetadataFinders.put("LOOKUP",
-                             new MetadataFinder("Lookup", 1));
-        sMetadataFinders.put("LOOKUP_TYPE",
-                             new MetadataFinder("LookupType", 2));
-        sMetadataFinders.put("VALIDATION_LOOKUP",
-                             new MetadataFinder("ValidationLookup", 1));
-        sMetadataFinders.put("VALIDATION_LOOKUP_TYPE",
-                             new MetadataFinder("ValidatoinLookupType", 2));
-        sMetadataFinders.put("VALIDATION_EXTERNAL",
-                             new MetadataFinder("ValidationExternal", 1));
-        sMetadataFinders.put("VALIDATION_EXTERNAL_TYPE",
-                             new MetadataFinder("ValidationExternalType", 2));
-        sMetadataFinders.put("VALIDATION_EXPRESSION",
-                             new MetadataFinder("ValidationExpression", 1));
-    }
+    private GetMetadataTransaction mTransaction;
+    private MetadataFetcher mMetadataFetcher;
+    private static final boolean USE_CACHE = true;
 }
