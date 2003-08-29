7b74ab84534992745949d07f125823cb225c41db
dterrell <dterrell@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-08-29 15:32:42 +0000

    add a new tag that does style="even" or style="odd" depending on a counter.

---
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/EvenOddTd.java
@@ -0,0 +1,87 @@
+package org.realtors.rets.server.webapp.cct;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.util.RequestUtils;
+
+/**
+ * @jsp:tag name="evenoddtd"
+ */
+public class EvenOddTd extends BodyTagSupport
+{
+    public EvenOddTd()
+    {
+        mCount = null;
+    }
+
+    /**
+     * @jsp:attribute required="true" rtexprvalue="false"
+     *  description="The counter bean"
+     * @return the name of the counter bean
+     */
+    public String getCount()
+    {
+        return mCount;
+    }
+
+    /**
+     *
+     * @param count the name of the counter bean
+     */
+    public void setCount(String count)
+    {
+        mCount = count;
+    }
+
+    public int doStartTag() throws JspException
+    {
+        JspWriter out = pageContext.getOut();
+        Integer counter = (Integer) RequestUtils.lookup(pageContext, mCount,
+                                                        null);
+        int count = counter.intValue();
+        String style;
+        if ((count % 2) == 1)
+        {
+            style = "odd";
+        }
+        else
+        {
+            style = "even";
+        }
+        try
+        {
+            out.write("<td style=\"");
+            out.write(style);
+            out.write("\">");
+        }
+        catch (IOException e)
+        {
+            LOG.error("An exception occured", e);
+            throw new JspException(e);
+        }
+        return EVAL_BODY_INCLUDE;
+    }
+
+    public int doEndTag() throws JspException
+    {
+        JspWriter out = pageContext.getOut();
+        try
+        {
+            out.write("</td>");
+        }
+        catch (IOException e)
+        {
+            LOG.error("An exception occured", e);
+            throw new JspException(e);
+
+        }
+        return EVAL_PAGE;
+    }
+
+    private String mCount;
+    private static final Logger LOG = Logger.getLogger(EvenOddTd.class);
+}
