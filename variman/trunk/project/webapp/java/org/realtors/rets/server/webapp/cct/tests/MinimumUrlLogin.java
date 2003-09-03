407cd2a6fd22578dbfa7b4208f01abd8d1b8fce4
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-03 20:20:52 +0000

    Add minimal URL login test

---
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/tests/MinimumUrlLogin.java
@@ -0,0 +1,64 @@
+/*
+ */
+package org.realtors.rets.server.webapp.cct.tests;
+
+import org.realtors.rets.server.cct.ValidationResults;
+import org.realtors.rets.server.webapp.cct.BaseCertificationTest;
+import org.realtors.rets.server.webapp.cct.HandlerManager;
+import org.realtors.rets.server.webapp.cct.LoginHandler;
+import org.realtors.rets.server.webapp.cct.InvokeCount;
+import org.realtors.rets.server.webapp.cct.ActionHandler;
+import org.realtors.rets.server.webapp.cct.LogoutHandler;
+import org.realtors.rets.server.webapp.cct.CapabilityUrlLevel;
+
+public class MinimumUrlLogin extends BaseCertificationTest
+{
+
+    public String getProcedure()
+    {
+        return "Login, then logout.";
+    }
+
+    protected ValidationResults validate()
+    {
+        ValidationResults results = new ValidationResults();
+        mLogin.validate(results);
+        mAction.validate(results);
+        mLogout.validate(results);
+        return results;
+    }
+
+    public String getDescription()
+    {
+        return "Tests a login with a minumum of capability URLs filled in";
+    }
+
+    public void start()
+    {
+        super.start();
+        HandlerManager actionManager = HandlerManager.getInstance();
+        mLogin = actionManager.getLoginHandler(mTestContext);
+        mLogin.reset();
+        mLogin.setCapabilityUrlLevel(CapabilityUrlLevel.MINIMAL);
+        mLogin.setSessionId(SESSION_ID);
+        mLogin.setGetInvokeCount(InvokeCount.ONE);
+        mLogin.addRequiredHeader("Accept", "^\\*/\\*$");
+        mLogin.addRequiredHeader("User-Agent", ".*");
+        mLogin.addRequiredHeader("RETS-Version", ".*");
+
+        mAction = actionManager.getActionHandler();
+        mAction.reset();
+        mAction.setGetInvokeCount(InvokeCount.ZERO);
+
+        mLogout = actionManager.getLogoutHandler();
+        mLogout.reset();
+        mLogout.setGetInvokeCount(InvokeCount.ZERO);
+
+        mStatus = RUNNING;
+    }
+
+    private LoginHandler mLogin;
+    private ActionHandler mAction;
+    private LogoutHandler mLogout;
+    public static final String SESSION_ID = "MinimalUrlLogin";
+}
