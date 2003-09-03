7170ddf1ce7d7c1857fa61a0b5eec244cc04c5fa
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-03 19:43:16 +0000

    Make logout transaction work

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/LogoutHandler.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/LogoutHandler.java
@@ -3,37 +3,37 @@
 package org.realtors.rets.server.webapp.cct;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.realtors.rets.server.cct.*;
+import org.realtors.rets.server.LogoutTransaction;
+import org.realtors.rets.server.AccountingStatistics;
 
 public class LogoutHandler extends BaseServletHandler
 {
     public static final String NAME = "/logout";
+
+    public LogoutHandler()
+    {
+        mTransaction = new LogoutTransaction();
+    }
 
     public String getName()
     {
         return NAME;
     }
 
-    public void reset()
-    {
-    }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
+        super.doGet(request, response);
+        response.setContentType("text/xml");
+        PrintWriter out = response.getWriter();
+        mTransaction.execute(out, new AccountingStatistics());
     }
 
-    public ValidationResults validate()
-    {
-        return null;
-    }
-
-    public void setGetInvokeCount(InvokeCount callCount)
-    {
-    }
+    private LogoutTransaction mTransaction;
 }
