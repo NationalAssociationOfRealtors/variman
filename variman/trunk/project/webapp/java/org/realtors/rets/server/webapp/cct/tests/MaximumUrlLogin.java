ffd1519cdf8db723ff4e0464e41e157cdca44013
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-03 20:20:30 +0000

    Add maximum URL login test

---
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/tests/MaximumUrlLogin.java
@@ -0,0 +1,72 @@
+/*
+ */
+package org.realtors.rets.server.webapp.cct.tests;
+
+import org.realtors.rets.server.cct.ValidationResults;
+import org.realtors.rets.server.webapp.cct.BaseCertificationTest;
+import org.realtors.rets.server.webapp.cct.HandlerManager;
+import org.realtors.rets.server.webapp.cct.LoginHandler;
+import org.realtors.rets.server.webapp.cct.InvokeCount;
+import org.realtors.rets.server.webapp.cct.ActionHandler;
+import org.realtors.rets.server.webapp.cct.LogoutHandler;
+import org.realtors.rets.server.webapp.cct.CapabilityUrlLevel;
+
+public class MaximumUrlLogin extends BaseCertificationTest
+{
+
+    public String getProcedure()
+    {
+        return "Login, then logout.";
+    }
+
+    protected ValidationResults validate()
+    {
+        ValidationResults results = new ValidationResults();
+        mLogin.validate(results);
+        mAction.validate(results);
+        mLogout.validate(results);
+        return results;
+    }
+
+    public String getDescription()
+    {
+        return "Tests a login with all capability URLs filled in";
+    }
+
+    public void start()
+    {
+        super.start();
+        HandlerManager actionManager = HandlerManager.getInstance();
+        mLogin = actionManager.getLoginHandler(mTestContext);
+        mLogin.reset();
+        mLogin.setCapabilityUrlLevel(CapabilityUrlLevel.MAXIMMAL);
+        mLogin.setSessionId(SESSION_ID);
+        mLogin.setGetInvokeCount(InvokeCount.ONE);
+        mLogin.addRequiredHeader("Accept", "^\\*/\\*$");
+        mLogin.addRequiredHeader("User-Agent", ".*");
+        mLogin.addRequiredHeader("RETS-Version", ".*");
+
+        mAction = actionManager.getActionHandler();
+        mAction.reset();
+        mAction.setGetInvokeCount(InvokeCount.ONE);
+        mAction.addRequiredHeader("Accept", "^\\*/\\*$");
+        mAction.addRequiredHeader("User-Agent", ".*");
+        mAction.addRequiredHeader("RETS-Version", ".*");
+        mAction.addCookie("RETS-Session-ID", "^" + SESSION_ID + "$");
+
+        mLogout = actionManager.getLogoutHandler();
+        mLogout.reset();
+        mLogout.setGetInvokeCount(InvokeCount.ZERO_OR_ONE);
+        mLogout.addRequiredHeader("Accept", "^\\*/\\*$");
+        mLogout.addRequiredHeader("User-Agent", ".*");
+        mLogout.addRequiredHeader("RETS-Version", ".*");
+        mLogout.addCookie("RETS-Session-ID", "^" + SESSION_ID + "$");
+
+        mStatus = RUNNING;
+    }
+
+    private LoginHandler mLogin;
+    private ActionHandler mAction;
+    private LogoutHandler mLogout;
+    public static final String SESSION_ID = "MinimalUrlLogin";
+}
