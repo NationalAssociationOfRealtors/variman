592eb2dbc09c46ffb3b1d0af7749024adcc59ae6
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-09 16:21:07 +0000

    Invalidate RETS session and remove cookie on logout

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/LogoutHandler.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/LogoutHandler.java
@@ -8,9 +8,11 @@ import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
 
 import org.realtors.rets.server.LogoutTransaction;
 import org.realtors.rets.server.AccountingStatistics;
+import org.realtors.rets.server.webapp.SessionFilter;
 
 public class LogoutHandler extends BaseServletHandler
 {
@@ -31,6 +33,14 @@ public class LogoutHandler extends BaseServletHandler
     {
         super.doGet(request, response);
         addRetsVersion(response);
+
+        SessionFilter.invalidateSession(request.getSession());
+        // Delete session cookie
+        Cookie cookie = new Cookie("RETS-Session-ID", "");
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
         response.setContentType("text/xml");
         PrintWriter out = response.getWriter();
         mTransaction.execute(out, new AccountingStatistics());
