25f8cbecbb3dddbdea546cadef426e46124a7ca0
kgarner <kgarner@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-17 19:28:15 +0000

    Added validateParameters and support for it

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/GetMetadataHandler.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/GetMetadataHandler.java
@@ -4,6 +4,8 @@ package org.realtors.rets.server.webapp.cct;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +16,8 @@ import org.realtors.rets.server.GetMetadataTransaction;
 import org.realtors.rets.server.ManagerMetadataFetcher;
 import org.realtors.rets.server.MetadataFetcher;
 import org.realtors.rets.server.RetsReplyException;
+import org.realtors.rets.server.cct.StatusEnum;
+import org.realtors.rets.server.cct.ValidationResult;
 import org.realtors.rets.server.metadata.MetadataManager;
 import org.realtors.rets.server.webapp.WebApp;
 
@@ -25,6 +29,25 @@ public class GetMetadataHandler extends BaseServletHandler
     {
         mTransaction = new GetMetadataTransaction();
         mMetadataFetcher = new ServletMetadataFetcher();
+        if (sMetadataTypes == null)
+        {
+            sMetadataTypes = new HashSet();
+            sMetadataTypes.add("METADATA-SYSTEM");
+            sMetadataTypes.add("METADATA-RESOURCE");
+            sMetadataTypes.add("METADATA-FOREIGN_KEY");
+            sMetadataTypes.add("METADATA-OBJECT");
+            sMetadataTypes.add("METADATA-LOOKUP");
+            sMetadataTypes.add("METADATA-LOOKUP_TYPE");
+            sMetadataTypes.add("METADATA-SEARCH_HELP");
+            sMetadataTypes.add("METADATA-EDITMASK");
+            sMetadataTypes.add("METADATA-VALIDATION_LOOKUP");
+            sMetadataTypes.add("METADATA-VALIDATION_LOOKUP_TYPE");
+            sMetadataTypes.add("METADATA-VALIDATION_EXTERNAL");
+            sMetadataTypes.add("METADATA-VALIDATION_EXPRESSION");
+            sMetadataTypes.add("METADATA-CLASS");
+            sMetadataTypes.add("METADATA-TABLE");
+            sMetadataTypes.add("METADATA-UPDATE_HELP");
+        }
     }
 
     public String getName()
@@ -69,7 +92,68 @@ public class GetMetadataHandler extends BaseServletHandler
             return WebApp.getMetadataManager();
         }
     }
+    
+    protected void validateParameters(ValidationResult result)
+    {
+        super.validateParameters(result);
 
+        // Required arguements
+        String type = (String) mActualParameters.get("Type");
+        if (type == null)
+        {
+            result.setStatus(StatusEnum.FAILED);
+            result.addMessage("getMetadata required parameter 'Type' missing.");
+        }
+        else
+        {
+            if (!sMetadataTypes.contains(type))
+            {
+                // We probably shouldn't drop a message here, but I'd like
+                // to test this some more.
+                result.addMessage(
+                    "Notice: getMetadata 'Type' not a standard type");
+            }
+        }
+
+        String id = (String) mActualParameters.get("ID");
+        if (id == null)
+        {
+            result.setStatus(StatusEnum.FAILED);
+            result.addMessage("getMetadata required parameter 'ID' missing.");
+        }
+        else
+        {
+            if (type != null &&
+                (type.equals("METADATA-SYSTEM") ||
+                type.equals("METADATA-RESOURCE")))
+            {
+                if (!(id.equals("*") || id.equals("0")))
+                {
+                    result.setStatus(StatusEnum.FAILED);
+                    result.addMessage(
+                        "getMetadata parameter 'ID' formatted incorrectly");
+                }
+            }
+        }
+        
+        // Optional arguements
+        String format = (String) mActualParameters.get("Format");
+        if (format != null)
+        {
+            boolean valid =
+                matches(format, "STANDARD-XML:RETS-METADATA-\\d{8}.dtd") ||
+                format.equals("COMPACT") ||
+                format.equals("STANDARD-XML");
+
+            if (!valid)
+            {
+                result.setStatus(StatusEnum.FAILED);
+                result.addMessage("getMetadata 'Format' parameter invalid");
+            }
+        }
+    }
+
+    private static Set sMetadataTypes = null;
     public static final String NAME = "/getMetadata";
     private static final Logger LOG =
         Logger.getLogger(GetMetadataHandler.class);
