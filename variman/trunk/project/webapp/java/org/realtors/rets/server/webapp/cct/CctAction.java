32e0c2b5e3bddd4a78cc2ea2dfe7787400ab75f1
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-22 20:21:36 +0000

    Pull up constants into an interface

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/CctAction.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/CctAction.java
@@ -2,9 +2,6 @@ package org.realtors.rets.server.webapp.cct;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import org.apache.struts.action.Action;
 
 /**
@@ -14,17 +11,12 @@ import org.apache.struts.action.Action;
  * Time: 2:05:42 PM
  * To change this template use Options | File Templates.
  */
-public abstract class CctAction extends Action
+public abstract class CctAction extends Action implements CctConstants
 {
     protected TestRunner getTestRunner(HttpSession session)
     {
         return (TestRunner) session.getAttribute(TESTRUNNER_KEY);
     }
     
-    private static final Log LOG = LogFactory.getLog(CctAction.class);
-    public static final String TESTRUNNER_KEY = "cctTestRunner";
-    public static final String USER_KEY = "cctUser";
-    public static final String USERINFO_KEY = "cctUserInfo";
-
     protected static final UserUtils UTILS = new UserUtils();
 }
