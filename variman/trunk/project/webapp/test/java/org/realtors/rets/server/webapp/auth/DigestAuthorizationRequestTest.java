6495360bdfef7294547aaf1cef5bc01cc04bfeec
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-22 20:19:31 +0000

    Add more tests from Showingtime's client

--- variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/auth/DigestAuthorizationRequestTest.java
+++ variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/auth/DigestAuthorizationRequestTest.java
@@ -93,6 +93,58 @@ public class DigestAuthorizationRequestTest extends TestCase
                                           true));
     }
 
+    /**
+     * Ensure that a header like Showingtime's with quotes around nc fail.
+     */
+    public void testShowingtimeHeader()
+    {
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest(
+            "Digest username=\"showingtime2\", realm=\"RETS Server\", " +
+            "nonce=\"27bc2a479bd8213202039dbc3ef5922b\", " +
+            "opaque=\"27bc2a479bd8213202039dbc3ef5922b\", " +
+            "uri=\"/rets/cct/login\", " +
+            "response=\"2c69a66c69996c5041f52697e70906b8\", qop=\"auth\", " +
+            "cnonce=\"0a4f113b\", nc=\"00000001\"");
+
+        assertEquals("showingtime2", request.getUsername());
+        assertEquals("RETS Server", request.getRealm());
+        assertEquals("27bc2a479bd8213202039dbc3ef5922b", request.getNonce());
+        assertEquals("/rets/cct/login", request.getUri());
+        assertEquals("auth", request.getQop());
+        assertEquals("HashUtils", request.getAlgorithm());
+        assertEquals("\"00000001\"", request.getNonceCount());
+        assertEquals("0a4f113b", request.getCnonce());
+        assertEquals("2c69a66c69996c5041f52697e70906b8", request.getResponse());
+        assertEquals("27bc2a479bd8213202039dbc3ef5922b", request.getOpaque());
+
+        request.setMethod("GET");
+        assertFalse(request.verifyResponse("stcom"));
+
+        // Test that removing quotes from nonce count results in a succesful
+        // verify.
+        request.setNonceCount("00000001");
+        assertTrue(request.verifyResponse("stcom"));
+    }
+
+    public void testShowingtimeResponse() throws NoSuchAlgorithmException
+    {
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest();
+        request.setUsername("showingtime2");
+        request.setRealm("RETS Server");
+        request.setMethod("GET");
+        request.setUri("/rets/cct/login");
+        request.setQop("auth");
+        request.setNonce("27bc2a479bd8213202039dbc3ef5922b");
+        request.setOpaque("27bc2a479bd8213202039dbc3ef5922b");
+        request.setCnonce("0a4f113b");
+        request.setNonceCount("00000001");
+        request.setResponse("2c69a66c69996c5041f52697e70906b8");
+        assertTrue(request.verifyResponse("stcom"));
+        // Test a pre-hashed A1 password
+        assertTrue(request.verifyResponse("9009f876938ab2dc5e71423bfe6dc0c2",
+                                          true));
+    }
+
     public void testNullPassword() throws NoSuchAlgorithmException
     {
         DigestAuthorizationRequest request = new DigestAuthorizationRequest();
