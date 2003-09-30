86e8fcf97f42e005348786ac018cb2a2fbd156b5
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-09-30 23:22:58 +0000

    Added Javadoc

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/auth/UserMap.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/auth/UserMap.java
@@ -6,5 +6,13 @@ import org.realtors.rets.server.User;
 
 public interface UserMap
 {
+    /**
+     * Finds a user object matching a username. <code>null</code> is returned
+     * if no matching user is found. If the username is <code>null</code>, a
+     * <code>null</code> <b>must</b> be returned.
+     *
+     * @param username The username to find
+     * @return The matching user object, or <code>null</code>
+     */
     User findUser(String username);
 }
