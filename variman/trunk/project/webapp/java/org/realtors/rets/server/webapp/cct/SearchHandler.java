f53df83d8d2f3f34d7c2faa1bd8f389db887eb53
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-27 16:33:08 +0000

    Use better exception handling

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/SearchHandler.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/cct/SearchHandler.java
@@ -5,14 +5,13 @@ package org.realtors.rets.server.webapp.cct;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import org.realtors.rets.server.RetsReplyException;
+import org.realtors.rets.server.RetsServerException;
 import org.realtors.rets.server.SearchParameters;
 import org.realtors.rets.server.SearchTransaction;
 import org.realtors.rets.server.webapp.RetsServletRequest;
 import org.realtors.rets.server.webapp.RetsServletResponse;
 import org.realtors.rets.server.webapp.WebApp;
 
-import antlr.ANTLRException;
 import org.apache.log4j.Logger;
 
 public class SearchHandler extends BaseServletHandler
@@ -24,25 +23,16 @@ public class SearchHandler extends BaseServletHandler
 
     protected void serviceRets(RetsServletRequest request,
                                RetsServletResponse response)
-        throws RetsReplyException, IOException
+        throws RetsServerException, IOException
     {
-        try
-        {
-            PrintWriter out = response.getXmlWriter();
-            SearchParameters parameters =
-                new SearchParameters(request.getParameterMap(),
-                                     request.getRetsVersion());
-            LOG.debug(parameters);
-            SearchTransaction search = new SearchTransaction(parameters);
-            LOG.debug(search.getSql(WebApp.getMetadataManager()));
-            out.println("<RETS ReplyCode=\"20201\" " +
-                        "ReplyText=\"No Records Found\"/>");
-        }
-        catch (ANTLRException e)
-        {
-            LOG.debug("Caught", e);
-            throw new RetsReplyException(20206, e.toString());
-        }
+        PrintWriter out = response.getXmlWriter();
+        SearchParameters parameters =
+            new SearchParameters(request.getParameterMap(),
+                                 request.getRetsVersion());
+        LOG.debug(parameters);
+        SearchTransaction search = new SearchTransaction(parameters);
+        search.setExecuteSql(false);
+        search.execute(out, WebApp.getMetadataManager(), WebApp.getSessions());
     }
 
     public static final String NAME = "/search";
