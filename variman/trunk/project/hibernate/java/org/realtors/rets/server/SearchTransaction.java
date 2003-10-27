eb55eb78638cb69f51d0929a3439d90e7f410182
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-27 16:23:09 +0000

    First pass implementation of search

--- variman/trunk/project/hibernate/java/org/realtors/rets/server/SearchTransaction.java
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/SearchTransaction.java
@@ -4,54 +4,148 @@ package org.realtors.rets.server;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
-//import net.sf.hibernate.HibernateException;
-//import net.sf.hibernate.Session;
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Session;
+import net.sf.hibernate.SessionFactory;
 
 import org.realtors.rets.server.dmql.DmqlCompiler;
 import org.realtors.rets.server.dmql.SqlConverter;
+import org.realtors.rets.server.metadata.MClass;
 import org.realtors.rets.server.metadata.MetadataManager;
 import org.realtors.rets.server.metadata.ServerDmqlMetadata;
-import org.realtors.rets.server.metadata.Table;
 
 import antlr.ANTLRException;
-//import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
-public class SearchTransaction
+public class SearchTransaction extends RetsTransaction
 {
     public SearchTransaction(SearchParameters parameters)
     {
         mParameters = parameters;
+        mExecuteSql = true;
     }
 
-    public String getQuery()
+    public void setExecuteSql(boolean executeSql)
     {
-        return mQuery;
+        mExecuteSql = executeSql;
     }
 
-    public void setQuery(String query)
+    public void execute(PrintWriter out, MetadataManager manager,
+                        SessionFactory sessions)
+        throws RetsServerException
     {
-        mQuery = query.trim();
-    }
-
-    public String getSql(MetadataManager manager) throws ANTLRException
-    {
-        List classes = manager.find(Table.TABLE,
-                                    mParameters.getResourceId() + ":" +
-                                    mParameters.getClassName());
+        MClass aClass = (MClass) manager.findByPath(
+            MClass.TABLE,
+            mParameters.getResourceId() + ":" + mParameters.getClassName());
         ServerDmqlMetadata metadata =
-            new ServerDmqlMetadata(classes, mParameters.isStandardNames());
-        SqlConverter sqlConverter = parse(metadata);
-        StringWriter stringWriter = new StringWriter();
-        sqlConverter.toSql(new PrintWriter(stringWriter));
-        return  stringWriter.toString();
+            new ServerDmqlMetadata(aClass, mParameters.isStandardNames());
+        Collection columns = metadata.getAllColumns();
+        String sql = generateSql(metadata, columns, aClass);
+        LOG.debug("SQL: " + sql);
+        Collection fields = columnsToFields(columns, metadata);
+        executeSql(sql, out, fields, sessions);
+    }
+
+    private String generateSql(ServerDmqlMetadata metadata, Collection columns,
+                               MClass aClass)
+        throws RetsReplyException
+    {
+        try
+        {
+            SqlConverter sqlConverter = parse(metadata);
+            StringWriter stringWriter = new StringWriter();
+            PrintWriter sqlWriter = new PrintWriter(stringWriter);
+            sqlWriter.print("SELECT ");
+            sqlWriter.print(StringUtils.join(columns.iterator(), ", "));
+            sqlWriter.print(" FROM ");
+            sqlWriter.print(aClass.getDbTable());
+            sqlWriter.print(" WHERE ");
+            sqlConverter.toSql(sqlWriter);
+            return stringWriter.toString();
+        }
+        catch (ANTLRException e)
+        {
+            // This is not an error as bad DMQL can cause this to be thrown.
+            LOG.debug("Caught", e);
+            throw new RetsReplyException(ReplyCode.INVALID_QUERY_SYNTAX,
+                                         e.toString());
+        }
+    }
+
+    private void executeSql(String sql, PrintWriter out, Collection fields,
+                            SessionFactory sessions)
+        throws RetsServerException
+    {
+        if (!mExecuteSql)
+        {
+            printEmptyRets(out, ReplyCode.NO_RECORDS_FOUND);
+            return;
+        }
+
+        Session session = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try
+        {
+            session = sessions.openSession();
+            Connection connection = session.connection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql.toString());
+            printOpenRetsSuccess(out);
+            out.println("<DELIMITER value=\"09\"/>");
+            out.print("<COLUMNS>\t");
+            out.print(StringUtils.join(fields.iterator(), "\t"));
+            out.println("\t</COLUMNS>");
+            int rowCount = 0;
+            while (resultSet.next())
+            {
+                out.print("<DATA>\t");
+                for (int i = 0; i < fields.size(); i++)
+                {
+                    out.print(resultSet.getString(i+1) + "\t");
+                }
+                out.println("\t</DATA>");
+                rowCount++;
+            }
+            printCloseRets(out);
+            LOG.debug("Row count: " + rowCount);
+        }
+        catch (HibernateException e)
+        {
+            throw new RetsServerException(e);
+        }
+        catch (SQLException e)
+        {
+            throw new RetsServerException(e);
+        }
+        finally
+        {
+            close(resultSet);
+            close(statement);
+            close(session);
+        }
+    }
+
+    private Collection columnsToFields(Collection columns,
+                                 ServerDmqlMetadata metadata)
+    {
+        List fields = new ArrayList();
+        for (Iterator i = columns.iterator(); i.hasNext();)
+        {
+            String column = (String) i.next();
+            fields.add(metadata.columnToField(column));
+        }
+        return fields;
     }
 
     private SqlConverter parse(ServerDmqlMetadata metadata)
@@ -69,72 +163,7 @@ public class SearchTransaction
         }
     }
 
-/*
-    public void doSearch(PrintWriter out, MetadataManager manager)
-        throws ANTLRException, HibernateException, SQLException
-    {
-        List tables = manager.find(Table.TABLE,
-                                   mParameters.getResourceId() +
-                                   ":" + mParameters.getClassName());
-        List columns = new ArrayList();
-        for (int i = 0; i < tables.size(); i++)
-        {
-            Table table = (Table) tables.get(i);
-            columns.add(table.getDbName());
-        }
-
-        StringBuffer sql = new StringBuffer();
-        sql.append("SELECT ");
-        sql.append(StringUtils.join(columns.iterator(), ", "));
-        sql.append(" FROM ");
-        sql.append(getSqlTable());
-        if (!mQuery.equals(""))
-        {
-            sql.append(" WHERE ");
-//            sql.append(getSql());
-        }
-        sql.append(";");
-
-        LOG.debug("SQL=" + sql.toString());
-
-        Session session = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try
-        {
-            session = InitServlet.openSession();
-            Connection connection = session.connection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(sql.toString());
-            out.println("<RETS ReplyCode=\"0\" " +
-                        "ReplyText=\"Operation Successful\">");
-            out.println("<DELIMITER value=\"09\"/>");
-            out.print("<COLUMNS>\t");
-            out.print(StringUtils.join(columns.iterator(), "\t"));
-            out.println("\t</COLUMNS>");
-            int rowCount = 0;
-            while (resultSet.next())
-            {
-                out.print("<DATA>\t");
-                for (int i = 0; i < columns.size(); i++)
-                {
-                    out.print(resultSet.getString(i+1) + "\t");
-                }
-                out.println("\t</DATA>");
-                rowCount++;
-            }
-            out.println("</RETS>");
-            LOG.debug("Row count: " + rowCount);
-        }
-        finally
-        {
-            closeResultSet(resultSet);
-            closeStatement(statement);
-            closeSession(session);
-        }
-    }
-
-    private void closeSession(Session session)
+    private void close(Session session)
     {
         try
         {
@@ -149,7 +178,7 @@ public class SearchTransaction
         }
     }
 
-    private void closeStatement(Statement statement)
+    private void close(Statement statement)
     {
         try
         {
@@ -164,7 +193,7 @@ public class SearchTransaction
         }
     }
 
-    private void closeResultSet(ResultSet resultSet)
+    private void close(ResultSet resultSet)
     {
         try
         {
@@ -179,17 +208,8 @@ public class SearchTransaction
         }
     }
 
-    private String getSqlTable()
-    {
-        return "rets_" + mParameters.getResourceId().toLowerCase() + "_" +
-            mParameters.getClassName().toLowerCase();
-    }
-
     private static final Logger LOG =
         Logger.getLogger(SearchTransaction.class);
-        */
-    private static final Logger LOG =
-        Logger.getLogger(SearchTransaction.class);
-    private String mQuery;
     private SearchParameters mParameters;
+    private boolean mExecuteSql;
 }
