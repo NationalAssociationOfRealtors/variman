94777378c8375cecb3b6f0453c83b6068cd1ed6e
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-28 17:23:16 +0000

    Move RetsTransaction to all static methods in RetsUtils

--- variman/trunk/project/hibernate/java/org/realtors/rets/server/RetsUtils.java
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/RetsUtils.java
@@ -4,48 +4,48 @@ package org.realtors.rets.server;
 
 import java.io.PrintWriter;
 
-public class RetsTransaction
+public class RetsUtils
 {
-    protected void printOpenRetsSuccess(PrintWriter out)
+    public static void printOpenRetsSuccess(PrintWriter out)
     {
         printOpenRets(out, 0, "Operation Successful");
     }
 
-    protected void printOpenRets(PrintWriter out, int replyCode,
-                                 String replyText)
+    public static void printOpenRets(PrintWriter out, int replyCode,
+                                     String replyText)
     {
         out.println("<RETS ReplyCode=\"" + replyCode + "\" " +
                     "ReplyText=\"" + replyText + "\">");
     }
 
-    protected void printOpenRets(PrintWriter out, ReplyCode replyCode)
+    public static void printOpenRets(PrintWriter out, ReplyCode replyCode)
     {
         printOpenRets(out, replyCode.getValue(), replyCode.getName());
     }
 
-    protected void printEmptyRets(PrintWriter out, int replyCode,
-                                 String replyText)
+    public static void printEmptyRets(PrintWriter out, int replyCode,
+                                      String replyText)
     {
         out.println("<RETS ReplyCode=\"" + replyCode + "\" " +
                     "ReplyText=\"" + replyText + "\"/>");
     }
 
-    protected void printEmptyRets(PrintWriter out, ReplyCode replyCode)
+    public static void printEmptyRets(PrintWriter out, ReplyCode replyCode)
     {
         printOpenRets(out, replyCode.getValue(), replyCode.getName());
     }
 
-    protected void printCloseRets(PrintWriter out)
+    public static void printCloseRets(PrintWriter out)
     {
         out.println("</RETS>");
     }
 
-    protected void printOpenRetsResponse(PrintWriter out)
+    public static void printOpenRetsResponse(PrintWriter out)
     {
         out.println("<RETS-RESPONSE>");
     }
 
-    protected void printCloseRetsResponse(PrintWriter out)
+    public static void printCloseRetsResponse(PrintWriter out)
     {
         out.println("</RETS-RESPONSE>");
     }
