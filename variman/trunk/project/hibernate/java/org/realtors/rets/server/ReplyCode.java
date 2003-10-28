43f9036174a7af9a4ef2aaa42b40cad6c27b58b6
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-28 17:21:35 +0000

    Addd 20203 and 20513

--- variman/trunk/project/hibernate/java/org/realtors/rets/server/ReplyCode.java
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/ReplyCode.java
@@ -8,8 +8,12 @@ public class ReplyCode extends ValuedEnum
 {
     public static final ReplyCode NO_RECORDS_FOUND =
         new ReplyCode("No Records Found", 20201);
+    public static final ReplyCode MISC_SEARCH_ERROR =
+        new ReplyCode("Miscellaneous Search Error", 20203);
     public static final ReplyCode INVALID_QUERY_SYNTAX =
         new ReplyCode("Invalid Query Syntax", 20206);
+    public static final ReplyCode MISC_ERROR =
+        new ReplyCode("Miscellaneous Error", 20513);
 
     private ReplyCode(String name, int value)
     {
