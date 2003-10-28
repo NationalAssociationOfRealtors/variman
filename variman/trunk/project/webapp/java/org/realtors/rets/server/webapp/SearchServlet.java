fe715bfa0e49f6f4cf2f1c2ddcdeb8d31cc8a71b
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-28 17:14:20 +0000

    Refactor to doRets() from RetsServlet

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/SearchServlet.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/SearchServlet.java
@@ -5,12 +5,7 @@ package org.realtors.rets.server.webapp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.realtors.rets.server.RetsReplyException;
-import org.realtors.rets.server.RetsVersion;
+import org.realtors.rets.server.RetsServerException;
 import org.realtors.rets.server.SearchParameters;
 import org.realtors.rets.server.SearchTransaction;
 
@@ -22,42 +17,17 @@ import org.apache.log4j.Logger;
  */
 public class SearchServlet extends RetsServlet
 {
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-        throws ServletException, IOException
+    protected void doRets(RetsServletRequest request,
+                          RetsServletResponse response)
+        throws RetsServerException, IOException
     {
-        response.setContentType("text/xml");
-        PrintWriter out = response.getWriter();
-        response.setBufferSize(64*1024);
-        LOG.debug("Buffer size: " + response.getBufferSize());
-
-        try
-        {
-            SearchParameters parameters =
-                new SearchParameters(request.getParameterMap(),
-                                     RetsVersion.RETS_1_0);
-            LOG.debug(parameters);
-            SearchTransaction search = new SearchTransaction(parameters);
-            search.execute(out, WebApp.getMetadataManager(),
-                           WebApp.getSessions());
-        }
-        catch (RetsReplyException e)
-        {
-            out.println("<RETS ReplyCode=\"" + e.getReplyCode() +
-                        "\" ReplyText=\"" + e.getMeaning() + "\"/>\n");
-        }
-        catch (Exception e)
-        {
-            LOG.error("Caught", e);
-            out.println("<RETS ReplyCode=\"20513\" " +
-                        "ReplyText=\"Miscellaneous error\"/>\n");
-        }
-    }
-
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException
-    {
-        doGet(req, resp);
+        PrintWriter out = response.getXmlWriter();
+        SearchParameters parameters =
+            new SearchParameters(request.getParameterMap(),
+                                 request.getRetsVersion());
+        LOG.debug(parameters);
+        SearchTransaction search = new SearchTransaction(parameters);
+        search.execute(out, WebApp.getMetadataManager(), WebApp.getSessions());
     }
 
     private static final Logger LOG =
