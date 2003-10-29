f8c810400a011faf9c731ba5981a349d6f36791a
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-29 18:57:04 +0000

    Revert from ReplyCode back to int/String for testing purposes

--- variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/cct/TestHandler.java
+++ variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/cct/TestHandler.java
@@ -5,10 +5,9 @@ package org.realtors.rets.server.webapp.cct;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.realtors.rets.server.RetsReplyException;
 import org.realtors.rets.server.webapp.RetsServletRequest;
 import org.realtors.rets.server.webapp.RetsServletResponse;
-import org.realtors.rets.server.RetsReplyException;
-import org.realtors.rets.server.ReplyCode;
 
 import org.apache.commons.lang.enum.Enum;
 
@@ -55,7 +54,7 @@ class TestHandler extends BaseServletHandler
 
         if (mExceptionMode == RETS_EXCEPTION)
         {
-            throw new RetsReplyException(ReplyCode.MISC_ERROR);
+            throw new RetsReplyException(10, "Error");
         }
         else if (mExceptionMode == RUNTIME_EXCEPTION)
         {
