9c3876ede3c33b7594d82a1f258949e4047ca03d
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-31 17:31:49 +0000

    Add fetchAllMetadata() and remove fetchMetadata()

--- variman/trunk/project/hibernate/java/org/realtors/rets/server/MetadataFetcher.java
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/MetadataFetcher.java
@@ -2,17 +2,21 @@
  */
 package org.realtors.rets.server;
 
+import java.util.List;
+
 import org.realtors.rets.server.metadata.MetadataSegment;
 
 public interface MetadataFetcher
 {
     /**
-     * Returns a list of metadata segments matching the specified criteria.
+     * Returns a list of metadata segments matching the specified type and
+     * levels.
      *
-     * @param type Metadata type
-     * @param levels Array of levels to start fetching metadata from
-     * @return A list of <code>MetadataSegment</code> objects.
+     * @param type Metadata type to fetch
+     * @param levels The levels to match
+     * @return A list of MetadataSegment objects
+     * @throws RetsServerException if an error occurs
      */
-    public MetadataSegment fetchMetadata(String type, String[] levels)
-        throws RetsReplyException;
+    public List fetchAllMetadata(String type, String[] levels)
+        throws RetsServerException;
 }
