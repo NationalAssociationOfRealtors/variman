817bbce0ca3e0af7a35accdcb3a0bd0858ce2a5d
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-10-31 17:32:25 +0000

    Add findByPattern()

--- variman/trunk/project/hibernate/java/org/realtors/rets/server/metadata/MetadataManager.java
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/metadata/MetadataManager.java
@@ -7,6 +7,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+import org.apache.commons.lang.StringUtils;
 
 public class MetadataManager
 {
@@ -30,13 +35,21 @@ public class MetadataManager
             table = new HashMap();
             mTables.put(metadata.getTableName(), table);
         }
-        List data = (List) table.get(metadata.getLevel());
+        List data = getList(table, metadata.getLevel());
+        data.add(metadata);
+//        data = getList(table, "*");
+//        data.add(metadata);
+    }
+
+    private List getList(Map table, String key)
+    {
+        List data = (List) table.get(key);
         if (data == null)
         {
             data = new ArrayList();
-            table.put(metadata.getLevel(), data);
+            table.put(key, data);
         }
-        data.add(metadata);
+        return data;
     }
 
     private void addByPath(ServerMetadata metadata)
@@ -57,6 +70,7 @@ public class MetadataManager
 
     public List find(String tableName, String level)
     {
+        LOG.debug("find: " + tableName + ", level: " + level);
         List found = Collections.EMPTY_LIST;
         Map table = (Map) mTables.get(tableName);
         if (table != null)
@@ -98,6 +112,94 @@ public class MetadataManager
         return metadata;
     }
 
+    public Map findByPattern(String tableName, String pattern)
+    {
+        String[] patternElements = StringUtils.split(pattern, ":");
+        return findByPattern(tableName, patternElements);
+    }
+
+    public Map findByPattern(String tableName, String[] patternElements)
+    {
+        Map found = new HashMap();
+
+        Map table = (Map) mTables.get(tableName);
+        if (table != null)
+        {
+            Set levels = table.keySet();
+            for (Iterator i = levels.iterator(); i.hasNext();)
+            {
+                String level = (String) i.next();
+                if (levelsMatch(level, patternElements))
+                {
+                    found.put(level, table.get(level));
+                }
+            }
+
+//            List data = getList(table, "*");
+//            for (int i = 0; i < data.size(); i++)
+//            {
+//                ServerMetadata metadata = (ServerMetadata) data.get(i);
+//                if (levelsMatch(metadata.getLevel(), patternElements))
+//                {
+//                    List atLevel = getList(found, metadata.getLevel());
+//                    atLevel.add(metadata);
+//                }
+//            }
+        }
+        return found;
+    }
+
+    public static boolean levelsMatch(String level, String pattern)
+    {
+        String[] patternElements = StringUtils.split(pattern, ":");
+        return levelsMatch(level, patternElements);
+    }
+
+    private static boolean levelsMatch(String level, String[] patternElements)
+    {
+        String[] levelElements = StringUtils.split(level, ":");
+        if (levelElements.length < patternElements.length)
+        {
+            return false;
+        }
+
+        // Pad "*" on front of short patterns so that the lenghts are equals
+        if (patternElements.length < levelElements.length)
+        {
+            String[] newPattern = new String[levelElements.length];
+            int numStarsToAdd = newPattern.length - patternElements.length;
+            for (int i = 0; i < newPattern.length; i++)
+            {
+                if (i < numStarsToAdd)
+                {
+                    newPattern[i] = "*";
+                }
+                else
+                {
+                    newPattern[i] = patternElements[i - numStarsToAdd];
+                }
+            }
+        }
+
+        boolean match = true;
+        for (int i = 0; i < levelElements.length && i < patternElements.length;
+             i++)
+        {
+            String levelElement = levelElements[i];
+            String patternElement = patternElements[i];
+            if (!patternElement.equals("*") &&
+                !patternElement.equals(levelElement))
+            {
+                match = false;
+                break;
+            }
+        }
+
+        return match;
+    }
+
+    private static final Logger LOG =
+        Logger.getLogger(MetadataManager.class);
     /**
      * A map of maps to lists:
      *
