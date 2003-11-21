57dfc1c7db579faaafdc552c82bea64da8c74541
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-11-21 23:20:51 +0000

    Add test for the stale flag, and remove test for generating nonces

--- variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/auth/DigestAuthenticateResponseTest.java
+++ variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/auth/DigestAuthenticateResponseTest.java
@@ -20,15 +20,19 @@ public class DigestAuthenticateResponseTest extends TestCase
                      "opaque=\"121d932ad13ff598b0df1d700e422812\"", header);
     }
 
-    public void testResponseHeaderGenerateNonce()
+    public void testStale()
     {
-        DigestAuthenticateResponse response1 =
+        DigestAuthenticateResponse response =
             new DigestAuthenticateResponse("RETS Server");
-        DigestAuthenticateResponse response2 =
-            new DigestAuthenticateResponse("RETS Server");
+        response.setNonce("246e1b9f80fd87d67c6eceffbcf89941");
+        response.setOpaque("121d932ad13ff598b0df1d700e422812");
+        response.setStale(true);
 
-        // nonce and opage should get generated
-        assertFalse(response1.getNonce().equals(response2.getNonce()));
-        assertFalse(response1.getOpaque().equals(response2.getOpaque()));
+        String header = response.getHeader();
+        assertEquals("Digest realm=\"RETS Server\", " +
+                     "qop=\"auth\", " +
+                     "nonce=\"246e1b9f80fd87d67c6eceffbcf89941\", " +
+                     "opaque=\"121d932ad13ff598b0df1d700e422812\", " +
+                     "stale=true", header);
     }
 }
