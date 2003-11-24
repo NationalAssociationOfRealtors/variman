0ec8d6f3835b47c665e322822177cf7720cf380f
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2003-11-24 23:55:28 +0000

    Have validateRequest() take a DigestAuthorizationRequest object, and have it check qop to validate as per RFC 2609 or 2617 accordingly.

--- variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/auth/NonceTableTest.java
+++ variman/trunk/project/webapp/test/java/org/realtors/rets/server/webapp/auth/NonceTableTest.java
@@ -21,20 +21,53 @@ public class NonceTableTest extends TestCase
         assertTrue(nonceTable.isValidNonce(nonce2));
     }
 
-    public void testNonceCountMustIncrease()
+    public void testValidateRfc2617Request()
     {
         NonceTable nonceTable = new NonceTable();
         String nonce = nonceTable.generateNonce();
-        assertTrue(nonceTable.isValidNonce(nonce));
         String opaque = nonceTable.getOpaque(nonce);
-        assertTrue(nonceTable.validateRequest(nonce, opaque, "00000001"));
-        assertTrue(nonceTable.validateRequest(nonce, opaque, "00000002"));
-        assertTrue(nonceTable.validateRequest(nonce, opaque, "0000000f"));
-        assertTrue(nonceTable.validateRequest(nonce, opaque, "00000010"));
-        assertFalse(nonceTable.validateRequest(nonce, opaque, "00000001"));
-        assertFalse(nonceTable.validateRequest(nonce, "", "f0000000"));
-        assertFalse(nonceTable.validateRequest("", opaque, "f0000000"));
-        assertTrue(nonceTable.validateRequest(nonce, opaque, "f0000000"));
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest();
+        request.setQop("auth");
+        request.setNonce(nonce);
+        request.setOpaque(opaque);
+        request.setNonceCount("00000001");
+        assertTrue(nonceTable.validateRequest(request));
+        request.setNonceCount("00000002");
+        assertTrue(nonceTable.validateRequest(request));
+        request.setNonceCount("0000000f");
+        assertTrue(nonceTable.validateRequest(request));
+        request.setNonceCount("00000010");
+        assertTrue(nonceTable.validateRequest(request));
+        // Try same nonce twice in a row
+        assertFalse(nonceTable.validateRequest(request));
+        // Try an old nonce count
+        request.setNonceCount("00000001");
+        assertFalse(nonceTable.validateRequest(request));
+        // Try no opaque value
+        request.setOpaque("");
+        assertFalse(nonceTable.validateRequest(request));
+        // Try no nonce value
+        request.setOpaque(opaque);
+        request.setNonce("");
+        assertFalse(nonceTable.validateRequest(request));
+        // Make sure the request validates, even after previous failures
+        request.setNonce(nonce);
+        request.setNonceCount("f0000000");
+        assertTrue(nonceTable.validateRequest(request));
+    }
+
+    public void testValidateRfc2609Request()
+    {
+        NonceTable nonceTable = new NonceTable();
+        String nonce = nonceTable.generateNonce();
+
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest();
+        request.setQop(null);
+        assertFalse(nonceTable.validateRequest(request));
+        request.setNonce(nonce);
+        assertTrue(nonceTable.validateRequest(request));
+        request.setQop("auth");
+        assertFalse(nonceTable.validateRequest(request));
     }
 
     public void testExpireNonces() throws InterruptedException
@@ -72,13 +105,57 @@ public class NonceTableTest extends TestCase
                     DateUtils.MILLIS_IN_MINUTE));
     }
 
-    public void testBadValidateExpirationTime()
+    public void testGood2617ValidateExpirationTime()
     {
         long time = System.currentTimeMillis();
         NonceTable nonceTable = new NonceTable();
         String nonce = nonceTable.generateNonce();
-        String opaque = nonceTable.getOpaque(nonce);
-        nonceTable.validateRequest(nonce, opaque, "00000000");
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest();
+        request.setQop("auth");
+        request.setNonce(nonce);
+        request.setOpaque(nonceTable.getOpaque(nonce));
+        request.setNonceCount("00000001");
+        nonceTable.validateRequest(request);
+        long expirationTime = nonceTable.getExpirationTime(nonce);
+        // Due to timing issues, the exact expiration time cannot be checked, so
+        // make sure it is within a minute of expected
+        assertTrue(expirationTime >=
+                   (time + NonceTable.DEFAULT_SUCCESS_TIMEOUT));
+        assertTrue(expirationTime <=
+                   (time + NonceTable.DEFAULT_SUCCESS_TIMEOUT +
+                    DateUtils.MILLIS_IN_MINUTE));
+    }
+
+    public void testGood2609ValidateExpirationTime()
+    {
+        long time = System.currentTimeMillis();
+        NonceTable nonceTable = new NonceTable();
+        String nonce = nonceTable.generateNonce();
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest();
+        request.setQop(null);
+        request.setNonce(nonce);
+        nonceTable.validateRequest(request);
+        long expirationTime = nonceTable.getExpirationTime(nonce);
+        // Due to timing issues, the exact expiration time cannot be checked, so
+        // make sure it is within a minute of expected
+        assertTrue(expirationTime >=
+                   (time + NonceTable.DEFAULT_SUCCESS_TIMEOUT));
+        assertTrue(expirationTime <=
+                   (time + NonceTable.DEFAULT_SUCCESS_TIMEOUT +
+                    DateUtils.MILLIS_IN_MINUTE));
+    }
+
+    public void testBad2617ValidateExpirationTime()
+    {
+        long time = System.currentTimeMillis();
+        NonceTable nonceTable = new NonceTable();
+        String nonce = nonceTable.generateNonce();
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest();
+        request.setQop("auth");
+        request.setNonce(nonce);
+        request.setOpaque(nonceTable.getOpaque(nonce));
+        request.setNonceCount("00000000");
+        nonceTable.validateRequest(request);
         long expirationTime = nonceTable.getExpirationTime(nonce);
         // Due to timing issues, the exact expiration time cannot be checked, so
         // make sure it is within a minute of expected
@@ -89,20 +166,22 @@ public class NonceTableTest extends TestCase
                     DateUtils.MILLIS_IN_MINUTE));
     }
 
-    public void testGoodValidateExpirationTime()
+    public void testBad2609ValidateExpirationTime()
     {
         long time = System.currentTimeMillis();
         NonceTable nonceTable = new NonceTable();
         String nonce = nonceTable.generateNonce();
-        String opaque = nonceTable.getOpaque(nonce);
-        nonceTable.validateRequest(nonce, opaque, "00000001");
+        DigestAuthorizationRequest request = new DigestAuthorizationRequest();
+        request.setQop(null);
+        request.setNonce(nonce + "foo");
+        nonceTable.validateRequest(request);
         long expirationTime = nonceTable.getExpirationTime(nonce);
         // Due to timing issues, the exact expiration time cannot be checked, so
         // make sure it is within a minute of expected
         assertTrue(expirationTime >=
-                   (time + NonceTable.DEFAULT_SUCCESS_TIMEOUT));
+                   (time + NonceTable.DEFAULT_INITIAL_TIMEOUT));
         assertTrue(expirationTime <=
-                   (time + NonceTable.DEFAULT_SUCCESS_TIMEOUT +
+                   (time + NonceTable.DEFAULT_INITIAL_TIMEOUT +
                     DateUtils.MILLIS_IN_MINUTE));
     }
 }
