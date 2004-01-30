d895065978f96a4a7bbd4a7f28adf825ed9e5431
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-01-30 18:03:33 +0000

    Test reading <database> tag

--- variman/trunk/project/hibernate/test/java/org/realtors/rets/server/config/RetsConfigTest.java
+++ variman/trunk/project/hibernate/test/java/org/realtors/rets/server/config/RetsConfigTest.java
@@ -2,38 +2,60 @@
  */
 package org.realtors.rets.server.config;
 
-import java.beans.IntrospectionException;
-import java.io.IOException;
-
 import org.realtors.rets.server.LinesEqualTestCase;
-
-import org.xml.sax.SAXException;
+import org.realtors.rets.server.RetsServerException;
 
 public class RetsConfigTest extends LinesEqualTestCase
 {
     public void testToXml()
-        throws IOException, SAXException, IntrospectionException
+        throws RetsServerException
     {
         RetsConfig retsConfig = new RetsConfig();
         retsConfig.setGetObjectRoot("/tmp/pictures");
         retsConfig.setGetObjectPattern("%k-%i.jpg");
         retsConfig.setNonceInitialTimeout(5);
         retsConfig.setNonceSuccessTimeout(10);
+
+        DatabaseConfig database = new DatabaseConfig();
+        database.setDriver("org.postgresql.Driver");
+        database.setUrl("jdbc:postgresql://localhost/rex_test");
+        database.setUsername("dave");
+        database.setPassword("");
+        database.setMaxActive(100);
+        database.setMaxWait(120000);
+        database.setMaxIdle(10);
+        database.setMaxPsActive(50);
+        database.setMaxPsWait(60000);
+        database.setMaxPsIdle(5);
+        retsConfig.setDatabase(database);
         String xml = retsConfig.toXml();
         assertLinesEqual(
-            "<?xml version='1.0' ?>\n" +
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
             "<rets-config>\n" +
             "  <get-object-pattern>%k-%i.jpg</get-object-pattern>\n" +
             "  <get-object-root>/tmp/pictures</get-object-root>\n" +
             "  <nonce-initial-timeout>5</nonce-initial-timeout>\n" +
             "  <nonce-success-timeout>10</nonce-success-timeout>\n" +
-            "</rets-config>",
+            "  <database>\n" +
+            "    <driver>org.postgresql.Driver</driver>\n" +
+            "    <url>jdbc:postgresql://localhost/rex_test</url>\n" +
+            "    <username>dave</username>\n" +
+            "    <password></password>\n" +
+            "    <max-active>100</max-active>\n" +
+            "    <max-idle>10</max-idle>\n" +
+            "    <max-wait>120000</max-wait>\n" +
+            "    <max-ps-active>50</max-ps-active>\n" +
+            "    <max-ps-idle>5</max-ps-idle>\n" +
+            "    <max-ps-wait>60000</max-ps-wait>\n" +
+            "  </database>\n" +
+            "</rets-config>\n" +
+            "\n",
             xml
         );
     }
 
     public void testFromXml()
-        throws SAXException, IOException, IntrospectionException
+        throws RetsServerException
     {
         String xml =
             "<?xml version='1.0' ?>\n" +
@@ -76,7 +98,7 @@ public class RetsConfigTest extends LinesEqualTestCase
     }
 
     public void testFromXmlDefaults()
-        throws SAXException, IOException, IntrospectionException
+        throws RetsServerException
     {
         String xml =
             "<?xml version='1.0' ?>\n" +
