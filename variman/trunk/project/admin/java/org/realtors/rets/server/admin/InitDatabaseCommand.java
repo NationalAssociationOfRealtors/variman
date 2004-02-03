da24d77c617c5c70dc00f85fd32df6d96b85f820
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-03 17:08:25 +0000

    Add dialog

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/InitDatabaseCommand.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/InitDatabaseCommand.java
@@ -10,6 +10,10 @@ package org.realtors.rets.server.admin;
 
 import org.wxwindows.wxWindowDisabler;
 import org.wxwindows.wxJWorker;
+import org.wxwindows.wxDialog;
+import org.wxwindows.wxWindow;
+import org.wxwindows.wxBoxSizer;
+import org.wxwindows.wxStaticText;
 
 import net.sf.hibernate.cfg.Configuration;
 import net.sf.hibernate.SessionFactory;
@@ -25,6 +29,8 @@ public class InitDatabaseCommand
     {
         final wxWindowDisabler disabler = new wxWindowDisabler();
         final AdminFrame frame = Admin.getAdminFrame();
+        final InitDatabaseDialog dialog = new InitDatabaseDialog(frame);
+        dialog.Show();
         frame.SetStatusText("Initializing database...");
         wxJWorker worker = new wxJWorker()
         {
@@ -56,11 +62,26 @@ public class InitDatabaseCommand
 
             public void finished()
             {
+                dialog.Destroy();
                 disabler.delete();
                 frame.SetStatusText("Database initialized successfully");
             }
         };
         worker.start();
+    }
+
+    private class InitDatabaseDialog extends wxDialog
+    {
+        public InitDatabaseDialog(wxWindow parent)
+        {
+            super(parent, -1, "Initializing Database");
+            wxBoxSizer box = new wxBoxSizer(wxVERTICAL);
+            wxStaticText label =
+                new wxStaticText(this, -1, "Initializing database...");
+            box.Add(label, 0, wxALL, 15);
+            SetSizer(box);
+            box.Fit(this);
+        }
     }
 
     private static final Logger LOG =
