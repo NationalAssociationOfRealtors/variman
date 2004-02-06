9712206f61ae86ecbd5a7e7c48d222921a90fa23
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-06 20:08:46 +0000

    Read metadata from a directory, rather than the database

--- variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/InitServlet.java
+++ variman/trunk/project/webapp/java/org/realtors/rets/server/webapp/InitServlet.java
@@ -11,32 +11,37 @@
 package org.realtors.rets.server.webapp;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import net.sf.hibernate.HibernateException;
-import net.sf.hibernate.Session;
 import net.sf.hibernate.cfg.Configuration;
 
+import org.realtors.rets.server.IOUtils;
+import org.realtors.rets.server.JdomUtils;
 import org.realtors.rets.server.PasswordMethod;
 import org.realtors.rets.server.RetsServerException;
-import org.realtors.rets.server.SessionHelper;
 import org.realtors.rets.server.config.RetsConfig;
 import org.realtors.rets.server.metadata.MSystem;
 import org.realtors.rets.server.metadata.MetadataManager;
 import org.realtors.rets.server.webapp.auth.NonceReaper;
 import org.realtors.rets.server.webapp.auth.NonceTable;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateUtils;
 import org.apache.log4j.Logger;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 
 /**
  * @web.servlet name="init-servlet"
@@ -180,37 +185,84 @@ public class InitServlet extends RetsServlet
         WebApp.setMetadataManager(manager);
     }
 
-    private MSystem findSystem()
-        throws ServletException
+    private MSystem findSystem() throws ServletException
     {
-        SessionHelper helper = WebApp.createHelper();
-        MSystem system = null;
         try
         {
-            Session session = helper.beginTransaction();
-            List results = new ArrayList();
-            Iterator iterator = session.iterate("from MSystem");
-            while (iterator.hasNext())
-            {
-                results.add(iterator.next());
-            }
-            if (results.size() == 1)
-            {
-                system = (MSystem) results.get(0);
-            }
-            helper.commit();
+            String metadataDir =
+                getContextInitParameter("rets-metadata-dir",
+                                        "WEB-INF/rex/metadata");
+            metadataDir = resolveFromConextRoot(metadataDir);
+            MetadataLoader loader = new MetadataLoader();
+            List files = IOUtils.listFilesRecursive(
+                new File(metadataDir), new MetadataFileFilter());
+            List documents = parseAllFiles(files);
+            Document merged =
+                JdomUtils.mergeDocuments(documents, new Element("RETS"));
+            return loader.parseMetadata(merged);
         }
-        catch (HibernateException e)
+        catch (JDOMException e)
         {
-            LOG.warn("Caught", e);
-            helper.rollback(LOG);
             throw new ServletException(e);
         }
-        finally
+        catch (IOException e)
         {
-            helper.close(LOG);
+            throw new ServletException(e);
         }
-        return system;
+    }
+
+    /**
+     * Parses all files, returning a list of JDOM Document objects.
+     *
+     * @param files list of File objects
+     * @return a list of Document objects
+     * @throws JDOMException if a JDOM error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private static List /* Document */ parseAllFiles(List /* File */ files)
+        throws JDOMException, IOException
+    {
+        List documents = new ArrayList();
+        SAXBuilder builder = new SAXBuilder();
+        for (int i = 0; i < files.size(); i++)
+        {
+            File file = (File) files.get(i);
+            documents.add(builder.build(file));
+        }
+        return documents;
+    }
+
+    /**
+     * Filters out directories and files that are not metadata, in particular
+     * files used by the 1.0 version of the RETS server. Metadata files must
+     * have a ".xml" extension. Certain directories, like Notices, Roles, and
+     * Template do not contain any metadata, so they are skipped completely.
+     */
+    private class MetadataFileFilter implements FileFilter
+    {
+        public boolean accept(File file)
+        {
+            if (file.isDirectory())
+            {
+                return false;
+            }
+
+            // These directories do not contain metadata
+            String parent = file.getParent();
+            if (StringUtils.contains(parent, "Notices") ||
+                StringUtils.contains(parent, "Roles") ||
+                StringUtils.contains(parent, "Template"))
+            {
+                return false;
+            }
+
+            if (file.getName().endsWith(".xml"))
+            {
+                return true;
+            }
+            // Everything else is not considered metadata
+            return false;
+        }
     }
 
     private void initNonceTable()
