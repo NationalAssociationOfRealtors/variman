af5ffc03631c8ca3618983e03626c18e61f8f3cf
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-06 20:32:54 +0000

    Add tests for listRecursive() methods

--- variman/trunk/project/hibernate/test/java/org/realtors/rets/server/IOUtilsTest.java
+++ variman/trunk/project/hibernate/test/java/org/realtors/rets/server/IOUtilsTest.java
@@ -2,9 +2,11 @@
  */
 package org.realtors.rets.server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -35,7 +37,48 @@ public class IOUtilsTest extends TestCase
         String file = getClass().getResource("foo.txt").getFile();
         file = StringUtils.replace(file, "foo.txt", "bar.txt");
         String expected = "a\nb\nc\n" + System.currentTimeMillis() + "\n";
-        IOUtils.writeString(file, expected);
+        IOUtils.writeString(expected, file);
         assertEquals(expected, IOUtils.readString(file));
+    }
+
+    public void testListRecursive()
+    {
+        String file = getClass().getResource("foo.txt").getFile();
+        file = StringUtils.replace(file, "foo.txt", "dirTree");
+        List files = IOUtils.listFilesRecursive(new File(file));
+        Collections.sort(files);
+        List expected = new ArrayList();
+        expected.add(new File(file, "a"));
+        expected.add(new File(file, "a/bar.ex2"));
+        expected.add(new File(file, "b"));
+        expected.add(new File(file, "b/baz.ex3"));
+        expected.add(new File(file, "foo.ex1"));
+        assertEquals(expected, files);
+    }
+
+    public void testListRecursiveFilenameFilter()
+    {
+        String file = getClass().getResource("foo.txt").getFile();
+        file = StringUtils.replace(file, "foo.txt", "dirTree");
+        List files = IOUtils.listFilesRecursive(
+            new File(file), new IOUtils.ExtensionFilter(".ex2"));
+        Collections.sort(files);
+        List expected = new ArrayList();
+        expected.add(new File(file, "a/bar.ex2"));
+        assertEquals(expected, files);
+    }
+
+    public void testListRecursiveFileFilter()
+    {
+        String file = getClass().getResource("foo.txt").getFile();
+        file = StringUtils.replace(file, "foo.txt", "dirTree");
+        List files = IOUtils.listFilesRecursive(
+            new File(file), new IOUtils.NotDirectoryFilter());
+        Collections.sort(files);
+        List expected = new ArrayList();
+        expected.add(new File(file, "a/bar.ex2"));
+        expected.add(new File(file, "b/baz.ex3"));
+        expected.add(new File(file, "foo.ex1"));
+        assertEquals(expected, files);
     }
 }
