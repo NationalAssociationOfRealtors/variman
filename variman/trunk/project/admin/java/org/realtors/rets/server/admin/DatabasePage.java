cbaada3a055008bb198d4bf061651cdef292d06d
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-09 17:43:01 +0000

    Add metadata directory

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/DatabasePage.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/DatabasePage.java
@@ -10,6 +10,7 @@ package org.realtors.rets.server.admin;
 
 import org.realtors.rets.server.config.DatabaseConfig;
 import org.realtors.rets.server.config.RetsConfig;
+import org.realtors.rets.server.IOUtils;
 
 import org.wxwindows.wxBoxSizer;
 import org.wxwindows.wxButton;
@@ -21,6 +22,7 @@ import org.wxwindows.wxStaticText;
 import org.wxwindows.wxWindow;
 import org.wxwindows.wxStaticLine;
 import org.wxwindows.wxTextCtrl;
+import org.wxwindows.wxDirDialog;
 
 import org.apache.commons.lang.math.NumberUtils;
 
@@ -36,7 +38,7 @@ public class DatabasePage extends wxPanel
         wxBoxSizer hBox;
 
         hBox = new wxBoxSizer(wxHORIZONTAL);
-        hBox.Add(new wxStaticText(this, -1, "Networking"), 0, wxALL, 5);
+        hBox.Add(new wxStaticText(this, -1, "RETS"), 0, wxALL, 5);
         hBox.Add(new wxStaticLine(this, -1), 1,
                  wxALIGN_CENTER_VERTICAL | wxALL, 5);
         box.Add(hBox, 0, wxEXPAND, 0);
@@ -46,12 +48,24 @@ public class DatabasePage extends wxPanel
 
         grid = new wxFlexGridSizer(0, 3, 0, 0);
         grid.AddGrowableCol(2);
-        label = new wxStaticText(this, -1, "Port:");
+        label = new wxStaticText(this, -1, "Listening Port:");
         grid.Add(label, 0, wxALIGN_LEFT | wxRIGHT | wxBOTTOM, 5);
         grid.Add(SPACER_WIDTH, -1);
         mPort = new wxTextCtrl(this, -1, "");
         grid.Add(mPort, 0, wxBOTTOM, 5);
+
+        label = new wxStaticText(this, -1, "Metadata Directory:");
+        grid.Add(label, 0, wxALIGN_LEFT | wxRIGHT | wxBOTTOM,  5);
+        grid.Add(SPACER_WIDTH, -1);
+        wxBoxSizer hBox2 = new wxBoxSizer(wxHORIZONTAL);
+        mMetadataDir = new wxTextCtrl(this, -1, "", wxDefaultPosition,
+                                      wxDefaultSize, wxTE_READONLY);
+        hBox2.Add(mMetadataDir, 1, wxEXPAND);
+        hBox2.Add(new wxButton(this, CHOOSE_METADATA_DIR, "Choose..."), 0,
+                  wxLEFT, 10);
+        grid.Add(hBox2, 0, wxEXPAND | wxBOTTOM, 5);
         hBox.Add(grid, 1, wxALL | wxEXPAND, 10);
+
         box.Add(hBox, 0, wxEXPAND, 0);
 
         hBox = new wxBoxSizer(wxHORIZONTAL);
@@ -95,6 +109,7 @@ public class DatabasePage extends wxPanel
         SetSizer(box);
 
         EVT_BUTTON(EDIT_PROPERTIES, new OnEditProperties());
+        EVT_BUTTON(CHOOSE_METADATA_DIR, new OnChooseMetadata());
     }
 
     private void updateLabels()
@@ -105,6 +120,9 @@ public class DatabasePage extends wxPanel
         mUrl.SetLabel(dbConfig.getUrl());
         mUsername.SetLabel(dbConfig.getUsername());
         mPort.SetValue("" + config.getPort());
+        String webappRoot = Admin.getWebappRoot();
+        mMetadataDir.SetValue(IOUtils.resolve(webappRoot,
+                                              config.getMetadataDir()));
     }
 
     public int getPort()
@@ -112,7 +130,7 @@ public class DatabasePage extends wxPanel
         return NumberUtils.stringToInt(mPort.GetValue());
     }
 
-    class OnEditProperties implements wxCommandListener
+    private class OnEditProperties implements wxCommandListener
     {
         public void handleEvent(wxCommandEvent event)
         {
@@ -138,11 +156,36 @@ public class DatabasePage extends wxPanel
         }
     }
 
+    private class OnChooseMetadata implements wxCommandListener
+    {
+        public void handleEvent(wxCommandEvent event)
+        {
+            RetsConfig config = Admin.getRetsConfig();
+            String webappRoot = Admin.getWebappRoot();
+            String metadataDir = config.getMetadataDir();
+            metadataDir = IOUtils.resolve(webappRoot, metadataDir);
+            wxDirDialog dirDialog =
+                new wxDirDialog(Admin.getAdminFrame(),
+                                "Choose Metadata Directory",
+                                metadataDir);
+            if (dirDialog.ShowModal() == wxID_OK)
+            {
+                metadataDir = dirDialog.GetPath();
+                metadataDir = IOUtils.relativize(webappRoot, metadataDir);
+                config.setMetadataDir(metadataDir);
+                updateLabels();
+            }
+            dirDialog.Destroy();
+        }
+    }
+
     private static final int EDIT_PROPERTIES = wxNewId();
+    private static final int CHOOSE_METADATA_DIR = wxNewId();
     private static final int SPACER_WIDTH = 10;
 
     private wxStaticText mDriverClass;
     private wxStaticText mUrl;
     private wxStaticText mUsername;
     private wxTextCtrl mPort;
+    private wxTextCtrl mMetadataDir;
 }
