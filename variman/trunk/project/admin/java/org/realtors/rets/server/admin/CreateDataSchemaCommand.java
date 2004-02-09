682db05a5ee7f79a4a89a057c755c6f68999e473
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-09 22:39:32 +0000

    Add debug commands

---
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/CreateDataSchemaCommand.java
@@ -0,0 +1,286 @@
+/*
+ * Rex RETS Server
+ *
+ * Author: Dave Dribin
+ * Copyright (c) 2004, The National Association of REALTORS
+ * Distributed under a BSD-style license.  See LICENSE.TXT for details.
+ */
+
+package org.realtors.rets.server.admin;
+
+import java.sql.Types;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Session;
+import net.sf.hibernate.SessionFactory;
+import net.sf.hibernate.dialect.Dialect;
+
+import org.realtors.rets.server.IOUtils;
+import org.realtors.rets.server.RetsServerException;
+import org.realtors.rets.server.metadata.InterpretationEnum;
+import org.realtors.rets.server.metadata.MClass;
+import org.realtors.rets.server.metadata.MSystem;
+import org.realtors.rets.server.metadata.MetadataLoader;
+import org.realtors.rets.server.metadata.Resource;
+import org.realtors.rets.server.metadata.Table;
+
+import org.apache.log4j.Logger;
+
+import org.wxwindows.wx;
+
+public class CreateDataSchemaCommand extends wx
+{
+    public CreateDataSchemaCommand()
+    {
+        mClasses = new HashMap();
+        mTables = new HashMap();
+        mLs = System.getProperty("line.separator");
+        mDialectClass = "net.sf.hibernate.dialect.PostgreSQLDialect";
+        mSessions = Admin.getSessionFactory();
+    }
+
+    public void execute()
+    {
+        try
+        {
+            loadMetadata();
+            String schema = createTables();
+            writeToDB(schema);
+        }
+        catch (Exception e)
+        {
+            LOG.error("Caught exception", e);
+        }
+    }
+
+    private void loadMetadata() throws RetsServerException
+    {
+        String metadataDir = Admin.getRetsConfig().getMetadataDir();
+        metadataDir = IOUtils.resolve(Admin.getWebappRoot(), metadataDir);
+        MetadataLoader loader = new MetadataLoader();
+        MSystem system = loader.parseMetadataDirectory(metadataDir);
+        System.out.println("Got system" + system.getId());
+        Iterator j = system.getResources().iterator();
+        while (j.hasNext())
+        {
+            Resource res = (Resource) j.next();
+            Iterator k = res.getClasses().iterator();
+            while (k.hasNext())
+            {
+                MClass clazz = (MClass) k.next();
+                mClasses.put(clazz.getPath(), clazz);
+                Iterator l = clazz.getTables().iterator();
+                while (l.hasNext())
+                {
+                    Table table = (Table) l.next();
+                    mTables.put(table.getPath(), table);
+                }
+            }
+        }
+    }
+
+    public String createTables()
+        throws InstantiationException, IllegalAccessException,
+               ClassNotFoundException, HibernateException
+    {
+        Dialect dialect = (Dialect)
+            getClass().getClassLoader().loadClass(mDialectClass).newInstance();
+        Iterator i = mClasses.values().iterator();
+        StringBuffer sb = new StringBuffer();
+        while (i.hasNext())
+        {
+            MClass clazz = (MClass) i.next();
+
+            String sqlTableName = clazz.getDbTable();
+
+            sb.append("CREATE TABLE ").append(sqlTableName);
+            sb.append(" (").append(mLs);
+            sb.append("\tid ").append(dialect.getTypeName(Types.BIGINT));
+            sb.append(" NOT NULL,").append(mLs);
+
+            boolean needsLookupMultiTable = false;
+            boolean firstDone = false;
+
+            Set needsIndex = new HashSet();
+            Iterator j = clazz.getTables().iterator();
+            while (j.hasNext())
+            {
+                Table table = (Table) j.next();
+                if (table.getInterpretation() ==
+                    InterpretationEnum.LOOKUPMULTI)
+                {
+                    needsLookupMultiTable = true;
+                }
+                else
+                {
+                    if (firstDone)
+                    {
+                        sb.append(",").append(mLs);
+                    }
+                    else
+                    {
+                        firstDone = true;
+                    }
+                    sb.append("\t").append(table.getDbName()).append(" ");
+                    switch (table.getDataType().toInt())
+                    {
+                        case 0 :
+                            sb.append(dialect.getTypeName(Types.BOOLEAN));
+                            break;
+                        case 1 :
+                            sb.append(
+                                dialect.getTypeName(Types.VARCHAR,
+                                                    table.getMaximumLength()));
+                            break;
+                        case 2 :
+                            sb.append(dialect.getTypeName(Types.DATE));
+                            break;
+                        case 3 :
+                            sb.append(dialect.getTypeName(Types.TIMESTAMP));
+                            break;
+                        case 4 :
+                            sb.append(dialect.getTypeName(Types.TIME));
+                            break;
+                        case 5 :
+                            sb.append(dialect.getTypeName(Types.TINYINT));
+                            break;
+                        case 6 :
+                            sb.append(dialect.getTypeName(Types.SMALLINT));
+                            break;
+                        case 7 :
+                            sb.append(dialect.getTypeName(Types.INTEGER));
+                            break;
+                        case 8 :
+                            sb.append(dialect.getTypeName(Types.BIGINT));
+                            break;
+                        case 9 :
+                            /* Not sure if this should be DECIMAL or NUMERIC */
+                            //sb.append(dialect.getTypeName(Types.DECIMAL));
+                            sb.append(dialect.getTypeName(Types.NUMERIC));
+                            break;
+                    }
+
+                    if (table.isUnique() && dialect.supportsUnique())
+                    {
+                        sb.append(" unique");
+                    }
+
+                    if (table.getIndex() > 0)
+                    {
+                        needsIndex.add(table);
+                    }
+                }
+            }
+
+            sb.append(mLs).append(");").append(mLs);
+            sb.append("alter table ").append(sqlTableName);
+            sb.append(dialect.getAddPrimaryKeyConstraintString(
+                      sqlTableName + "_pk_id"));
+            sb.append("(id);").append(mLs);
+
+            j = needsIndex.iterator();
+            while (j.hasNext())
+            {
+                Table table = (Table) j.next();
+                String dbName = table.getDbName();
+                sb.append("create index ").append(sqlTableName).append("_");
+                sb.append(dbName).append("_index on ").append(sqlTableName);
+                sb.append("(").append(dbName).append(");").append(mLs);
+            }
+
+            if (needsLookupMultiTable)
+            {
+                String bigint = dialect.getTypeName(Types.BIGINT);
+                String lmTable = sqlTableName + "_lm";
+                sb.append("create table ").append(lmTable).append(" (");
+                sb.append(mLs);
+                sb.append("\tid ").append(bigint).append(" NOT NULL,");
+                sb.append(mLs);
+                sb.append("\tparent_id ").append(bigint).append(" NOT NULL, ");
+                sb.append(mLs);
+                sb.append("\tlookup_name ");
+                sb.append(dialect.getTypeName(Types.VARCHAR, 32));
+                sb.append(" NOT NULL, ").append(mLs);
+                sb.append("\tvalue ");
+                sb.append(dialect.getTypeName(Types.VARCHAR, 32)).append(mLs);
+                sb.append(");").append(mLs);
+
+                // Do the primary key
+                sb.append("alter table ").append(lmTable);
+                sb.append(dialect.getAddPrimaryKeyConstraintString(
+                    lmTable + "_pk_id"));
+                sb.append("(id);").append(mLs);
+
+                // do the foreign key
+                sb.append("alter table ").append(lmTable);
+                sb.append(
+                    dialect.getAddForeignKeyConstraintString(lmTable + "_fk",
+                        new String[] { "parent_id" }, sqlTableName,
+                        new String[] { "id" })).append(";").append(mLs);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Write the schema out to the database.
+     *
+     * @param schema the schema to put into the db
+     */
+    private void writeToDB(String schema)
+        throws HibernateException, SQLException
+    {
+        Session session = null;
+        Connection con = null;
+        try
+        {
+            session = mSessions.openSession();
+            con = session.connection();
+            Statement stmt = con.createStatement();
+            stmt.execute(schema);
+            con.commit();
+            session.close();
+        }
+        catch (HibernateException e)
+        {
+            if (con != null)
+            {
+                con.rollback();
+            }
+            if (session != null)
+            {
+                session.close();
+            }
+            throw e;
+        }
+        catch (SQLException e)
+        {
+            if (con != null)
+            {
+                con.rollback();
+            }
+            if (session != null)
+            {
+                session.close();
+            }
+            throw e;
+        }
+    }
+
+    private static final Logger LOG =
+        Logger.getLogger(CreateDataSchemaCommand.class);
+    private Map mClasses;
+    private Map mTables;
+    private String mLs;
+    private String mDialectClass;
+    private SessionFactory mSessions;
+}
---
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/CreatePropertiesCommand.java
@@ -0,0 +1,348 @@
+/*
+ * Rex RETS Server
+ *
+ * Author: Dave Dribin
+ * Copyright (c) 2004, The National Association of REALTORS
+ * Distributed under a BSD-style license.  See LICENSE.TXT for details.
+ */
+
+package org.realtors.rets.server.admin;
+
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Calendar;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.Date;
+
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Session;
+import net.sf.hibernate.SessionFactory;
+
+import org.realtors.rets.server.RetsServerException;
+import org.realtors.rets.server.IOUtils;
+import org.realtors.rets.server.metadata.MetadataLoader;
+import org.realtors.rets.server.metadata.MSystem;
+import org.realtors.rets.server.metadata.Resource;
+import org.realtors.rets.server.metadata.MClass;
+import org.realtors.rets.server.metadata.Table;
+
+import org.apache.log4j.Logger;
+
+public class CreatePropertiesCommand
+{
+    public CreatePropertiesCommand(int numProperties)
+    {
+        mClasses = new HashMap();
+        mTables = new HashMap();
+        mSessions = Admin.getSessionFactory();
+        mNumProperties = numProperties;
+        initData();
+    }
+
+    private void initData()
+    {
+        mRandom = new Random(System.currentTimeMillis());
+        mRandom.nextInt();
+
+        mCount = 0;
+        mESchools = new int[] { 306, 300, 42, 304, 303 };
+        mAgents = new String[] { "P345", "P123", "M123" };
+        mBrokers = new String[] { "Laffalot Realty", "Tex Mex Real Estate",
+            "Yellow Armadillo Realty", "Retzilla Realty" };
+        mStreets = new String[] { "Buckingham Dr.", "Main St.",
+            "Knoll Creek Dr.", "Randall Rd.", "Exeter Ct.", "Anderson Blvd.",
+            "Westminster Circle" };
+        mUrls = new String[] { "http://www.crt.realtors.org/",
+            "http://www.realtors.org", "http://slashdot.org/" };
+        mListingTypes = new String[] { "INC", "LND", "RENT", "RES" };
+        mLocations = new String[] { "AUR", "BATV", "ELBRN", "ELGN", "GENVA",
+            "SELGN", "STC", "WYNE", "WCHIC" };
+        mStatus = new String[] { "A", "C", "X", "L", "O", "P", "S", "T", "U",
+            "W" };
+        mOwners = new String[] { "Keith Garner", "Dave Dribin",
+            "Dave Terrell", "Mark Lesswing" };
+
+        mDate = new Date[4];
+        Calendar cal = Calendar.getInstance();
+        cal.set(2003, 6, 10);
+        mDate[mCount++] = new Date(cal.getTime().getTime());
+        cal.set(2003, 6, 22);
+        mDate[mCount++] = new Date(cal.getTime().getTime());
+        cal.set(2002, 11, 5);
+        mDate[mCount++] = new Date(cal.getTime().getTime());
+        mDate[mCount++] = new Date(new java.util.Date().getTime());
+    }
+
+    public void execute()
+    {
+        try
+        {
+            loadMetadata();
+            dropData();
+            createData();
+        }
+        catch (Exception e)
+        {
+            LOG.error("Caught exception", e);
+        }
+    }
+
+    private void loadMetadata() throws RetsServerException
+    {
+        String metadataDir = Admin.getRetsConfig().getMetadataDir();
+        metadataDir = IOUtils.resolve(Admin.getWebappRoot(), metadataDir);
+        MetadataLoader loader = new MetadataLoader();
+        MSystem system = loader.parseMetadataDirectory(metadataDir);
+        System.out.println("Got system" + system.getId());
+        Iterator j = system.getResources().iterator();
+        while (j.hasNext())
+        {
+            Resource res = (Resource) j.next();
+            Iterator k = res.getClasses().iterator();
+            while (k.hasNext())
+            {
+                MClass clazz = (MClass) k.next();
+                mClasses.put(clazz.getPath(), clazz);
+                Iterator l = clazz.getTables().iterator();
+                while (l.hasNext())
+                {
+                    Table table = (Table) l.next();
+                    mTables.put(table.getPath(), table);
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     *
+     */
+    private void dropData() throws HibernateException, SQLException
+    {
+        Session session = null;
+        Connection con = null;
+        Statement stmt = null;
+        try
+        {
+            session = mSessions.openSession();
+            con = session.connection();
+            stmt = con.createStatement();
+
+            stmt.execute("delete from rets_property_res");
+
+            con.commit();
+        }
+        catch (HibernateException e)
+        {
+            if (con != null)
+            {
+                con.rollback();
+            }
+            if (session != null)
+            {
+                session.close();
+            }
+            throw e;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(stmt);
+            if (con != null)
+            {
+                con.rollback();
+            }
+            if (session != null)
+            {
+                session.close();
+            }
+            throw e;
+        }
+    }
+
+    private void createData() throws HibernateException, SQLException
+    {
+        Session session = null;
+        Connection con = null;
+        PreparedStatement ps = null;
+        try
+        {
+            session = mSessions.openSession();
+            con = session.connection();
+            con.setAutoCommit(false);
+
+            for (int i = 0; i < mNumProperties; i++)
+            {
+                ps = con.prepareStatement(
+                    "INSERT INTO " +
+                    "rets_property_res(id,r_lp,r_broker,r_agent_id,r_ln," +
+                    "                  r_zip_code,r_ld,r_sqft,r_e_school," +
+                    "                  r_m_school,r_h_school,r_stname,r_url," +
+                    "                  r_ltyp, r_stnum, r_vew, r_ar,r_status," +
+                    "                  r_owner)" +
+                    " VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
+                );
+                // todo: LookupMulti EF needs to be added.
+                // todo: LookupMulti IF needs to be added.
+                mCount += mRandom.nextInt(10);
+                ps.setInt(1, i);
+                ps.setInt(2, mRandom.nextInt(1000000));
+
+                ps.setString(3, getNextBroker());
+                ps.setString(4, getNextAgent());
+
+                ps.setString(5, "LN" + Long.toHexString(i));
+
+                ps.setInt(6, mRandom.nextInt(99999));
+                ps.setDate(7, getNextDate());
+
+                ps.setString(8, Long.toString(mRandom.nextInt(7000)));
+                ps.setInt(9, getNextSchool());
+                ps.setInt(10, getNextSchool());
+                ps.setInt(11, getNextSchool());
+
+                ps.setString(12, getNextStreet());
+                ps.setString(13, getNextURL());
+                ps.setString(14, getNextListingType());
+                ps.setString(15, Long.toString(mRandom.nextInt(9999999)));
+                ps.setInt(16, mRandom.nextInt(2048));
+                ps.setString(17, getNextLocation());
+                ps.setString(18, getNextStatus());
+                ps.setString(19, getNextOwner());
+                ps.execute();
+            }
+            con.commit();
+            session.close();
+        }
+        catch (HibernateException e)
+        {
+            if (con != null)
+            {
+                con.rollback();
+            }
+            if (session != null)
+            {
+                session.close();
+            }
+            throw e;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(ps);
+            if (con != null)
+            {
+                con.rollback();
+            }
+            if (session != null)
+            {
+                session.close();
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * @return a String from the agent array
+     */
+    private String getNextAgent()
+    {
+        return mAgents[mCount++ % mAgents.length];
+    }
+
+    /**
+     * @return a String from the broker array
+     */
+    private String getNextBroker()
+    {
+        return mBrokers[mCount++ % mBrokers.length];
+    }
+
+    /**
+     * @return a Date from the date array
+     */
+    private Date getNextDate()
+    {
+        return mDate[mCount++ % mDate.length];
+    }
+
+    /**
+     * @return
+     */
+    private String getNextListingType()
+    {
+        return mListingTypes[mCount++ % mListingTypes.length];
+    }
+
+    /**
+     * @return
+     */
+    private String getNextLocation()
+    {
+        return mLocations[mCount++ % mLocations.length];
+    }
+
+    /**
+     * @return
+     */
+    private String getNextOwner()
+    {
+        return mOwners[mCount++ % mOwners.length];
+    }
+
+    /**
+     * @return ant int of the next school
+     */
+    private int getNextSchool()
+    {
+        return mESchools[mCount++ % mESchools.length];
+    }
+
+    /**
+     * @return
+     */
+    private String getNextStatus()
+    {
+        return mStatus[mCount++ % mStatus.length];
+    }
+
+    /**
+     * @return a string with the street
+     */
+    private String getNextStreet()
+    {
+        return mStreets[mCount++ % mStreets.length];
+    }
+
+    /**
+     * @return a String from the URL array
+     */
+    private String getNextURL()
+    {
+        return mUrls[mCount++ % mUrls.length];
+    }
+
+
+    private static final Logger LOG =
+        Logger.getLogger(CreatePropertiesCommand.class);
+    private Map mClasses;
+    private Map mTables;
+    private int mNumProperties;
+    private SessionFactory mSessions;
+
+    private String[] mAgents;
+    private String[] mBrokers;
+    private int mCount;
+    private Date[] mDate;
+    private int[] mESchools;
+    private String[] mListingTypes;
+    private String[] mLocations;
+    private String[] mOwners;
+    private Random mRandom;
+    private String[] mStatus;
+    private String[] mStreets;
+    private String[] mUrls;
+}
