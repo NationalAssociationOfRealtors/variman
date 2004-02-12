cd63914c24b70b1a8292f9d44f1a377f49f011ec
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-12 20:51:14 +0000

    Check if user exists, prior to adding

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/AddUserCommand.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/AddUserCommand.java
@@ -13,6 +13,7 @@ import java.io.StringWriter;
 
 import net.sf.hibernate.HibernateException;
 
+import org.realtors.rets.server.HibernateUtils;
 import org.realtors.rets.server.User;
 import org.realtors.rets.server.UserUtils;
 
@@ -25,23 +26,22 @@ public class AddUserCommand extends wx
 {
     public void execute()
     {
+        AddUserDialog dialog = null;
         try
         {
             AdminFrame frame = Admin.getAdminFrame();
-            AddUserDialog addUserDialog = new AddUserDialog(frame);
-            int response = addUserDialog.ShowModal();
-            addUserDialog.Destroy();
-            if (response == wxID_CANCEL)
+            dialog = new AddUserDialog(frame);
+            if (showUntilCancelOrValid(dialog) != wxID_OK)
             {
                 return;
             }
 
             User user = new User();
-            user.setFirstName(addUserDialog.getFirstName());
-            user.setLastName(addUserDialog.getLastName());
-            user.setUsername(addUserDialog.getUsername());
-            user.changePassword(addUserDialog.getPassword());
-            UserUtils.save(user);
+            user.setFirstName(dialog.getFirstName());
+            user.setLastName(dialog.getLastName());
+            user.setUsername(dialog.getUsername());
+            user.changePassword(dialog.getPassword());
+            HibernateUtils.save(user);
             frame.SetStatusText("User " + user.getName() + " added");
             frame.refreshUsers();
             LOG.debug("New user: " + user);
@@ -56,6 +56,37 @@ public class AddUserCommand extends wx
             wxLogMessage(stackTrace);
             wxLogError("Could not add user.");
         }
+        finally
+        {
+            if (dialog != null)
+            {
+                dialog.Destroy();
+            }
+        }
+    }
+
+    private int showUntilCancelOrValid(AddUserDialog dialog)
+        throws HibernateException
+    {
+        int response = wxID_CANCEL;
+        while (true)
+        {
+            response = dialog.ShowModal();
+            if (response == wxID_CANCEL)
+            {
+                break;
+            }
+
+            String username = dialog.getUsername();
+            if (UserUtils.findByUsername(username) == null)
+            {
+                break;
+            }
+            wxMessageBox("A user already exists with this username.\n" +
+                         "Please choose a new username.", "Error",
+                         wxOK | wxICON_EXCLAMATION, Admin.getAdminFrame());
+        };
+        return response;
     }
 
     private static final Logger LOG =
