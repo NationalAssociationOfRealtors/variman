7cd4403f0760741907f2b8ac716cd7394af712b4
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-19 23:21:53 +0000

    Display number of test classes found

--- variman/trunk/project/hibernate/test/java/test/AllTests.java
+++ variman/trunk/project/hibernate/test/java/test/AllTests.java
@@ -3,14 +3,14 @@
 package test;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.lang.reflect.Modifier;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.apache.commons.lang.SystemUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.SystemUtils;
 
 public class AllTests
 {
@@ -19,34 +19,37 @@ public class AllTests
         TestSuite suite = new TestSuite();
         String[] paths = StringUtils.split(SystemUtils.JAVA_CLASS_PATH,
                                            SystemUtils.PATH_SEPARATOR);
+        int numSuites = 0;
         for (int i = 0; i < paths.length; i++)
         {
             String path = paths[i];
-            addSuitesFromPath(suite, path);
+            numSuites += addSuitesFromPath(suite, path);
         }
+        System.out.println(numSuites + " test classes found");
         return suite;
     }
 
-    private static void addSuitesFromPath(TestSuite suite, String path)
+    private static int addSuitesFromPath(TestSuite suite, String path)
     {
         if (path.endsWith(".jar"))
         {
-            return;
+            return 0;
         }
         File file = new File(path);
-        addSuitesFromFile(suite, file, "");
+        return addSuitesFromDirectory(suite, file, "");
     }
 
-    private static void addSuitesFromFile(TestSuite suite, File file,
-                                          String pkg)
+    private static int addSuitesFromDirectory(TestSuite suite, File directory,
+                                              String pkg)
     {
-        String[] classNames = listClasses(file);
+        String[] classNames = listClasses(directory);
+        int numSuites = 0;
         for (int i = 0; i < classNames.length; i++)
         {
             String className = classNames[i];
             // Rip off trailing ".class"
-            className = className.substring(
-                0, className.length() - ".class".length());
+            int suffixIndex = className.length() - ".class".length();
+            className = className.substring(0, suffixIndex);
             if (!className.endsWith("Test"))
             {
                 continue;
@@ -59,6 +62,7 @@ public class AllTests
                 {
                     suite.addTestSuite(clazz);
 //                    System.out.println("Adding: " + className);
+                    numSuites++;
                 }
             }
             catch (ClassNotFoundException e)
@@ -67,13 +71,15 @@ public class AllTests
             }
         }
 
-        File[] directories = listDirectories(file);
-        for (int i = 0; i < directories.length; i++)
+        File[] subdirectories = listDirectories(directory);
+        for (int i = 0; i < subdirectories.length; i++)
         {
-            File directory = directories[i];
-            String newPkg = appendPackage(pkg, directory.getName());
-            addSuitesFromFile(suite, directory, newPkg);
+            File subdirectory = subdirectories[i];
+            String newPkg = appendPackage(pkg, subdirectory.getName());
+            numSuites +=
+                addSuitesFromDirectory(suite, subdirectory, newPkg);
         }
+        return numSuites;
     }
 
     private static String appendPackage(String pkg, String element)
@@ -90,7 +96,8 @@ public class AllTests
 
     private static File[] listDirectories(File file)
     {
-        File[] directories = file.listFiles(new FileFilter() {
+        File[] directories = file.listFiles(new FileFilter()
+        {
             public boolean accept(File pathname)
             {
                 if (pathname.isDirectory())
@@ -108,7 +115,8 @@ public class AllTests
 
     private static String[] listClasses(File file)
     {
-        String[] classNames = file.list(new FilenameFilter() {
+        String[] classNames = file.list(new FilenameFilter()
+        {
             public boolean accept(File dir, String name)
             {
                 if (name.endsWith(".class"))
