337be0fb6824e89b65925ba6d36a3ee070991013
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-02-27 18:23:43 +0000

    Add some helper methods

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/metadata/AbstractSubPanel.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/metadata/AbstractSubPanel.java
@@ -11,8 +11,11 @@ package org.realtors.rets.server.admin.metadata;
 import org.wxwindows.wxScrolledWindow;
 import org.wxwindows.wxWindow;
 import org.wxwindows.wxBoxSizer;
+import org.wxwindows.wxChoice;
+import org.wxwindows.wxTextCtrl;
 
 import org.realtors.rets.server.admin.TwoColumnGridSizer;
+import org.realtors.rets.server.admin.BooleanChoice;
 
 public abstract class AbstractSubPanel extends wxScrolledWindow
 {
@@ -30,4 +33,43 @@ public abstract class AbstractSubPanel extends wxScrolledWindow
     }
 
     protected abstract void addContent(TwoColumnGridSizer grid);
+
+    public static void setValue(wxTextCtrl textCtrl, Object text)
+    {
+        if (text == null)
+        {
+            textCtrl.SetValue("");
+        }
+        else
+        {
+            textCtrl.SetValue(text.toString());
+        }
+    }
+
+    public static void setValue(wxTextCtrl textCtrl, int number)
+    {
+        textCtrl.SetValue(Integer.toString(number));
+    }
+
+    public static void setValue(wxTextCtrl textCtrl, boolean b)
+    {
+        textCtrl.SetValue(Boolean.toString(b));
+    }
+
+    public static void setValue(wxChoice choice, Object object)
+    {
+        if (object == null)
+        {
+            choice.SetStringSelection("<none>");
+        }
+        else
+        {
+            choice.SetStringSelection(object.toString());
+        }
+    }
+
+    public void setValue(BooleanChoice choice, boolean b)
+    {
+        choice.SetBooleanSelection(b);
+    }
 }
