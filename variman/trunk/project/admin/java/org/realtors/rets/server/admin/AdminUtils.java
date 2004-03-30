d264c19423018f8ea8f3cc65b02a5816be2175eb
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-03-30 16:39:49 +0000

    Add initConfig() and initDatabase()

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/AdminUtils.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/AdminUtils.java
@@ -8,7 +8,19 @@
 
 package org.realtors.rets.server.admin;
 
+import java.io.File;
+
 import org.wxwindows.wxTextCtrl;
+
+import org.apache.log4j.Logger;
+
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.SessionFactory;
+import net.sf.hibernate.cfg.Configuration;
+import org.realtors.rets.server.PasswordMethod;
+import org.realtors.rets.server.RetsServer;
+import org.realtors.rets.server.RetsServerException;
+import org.realtors.rets.server.config.RetsConfig;
 
 public class AdminUtils
 {
@@ -33,4 +45,32 @@ public class AdminUtils
     {
         textCtrl.SetValue(Boolean.toString(b));
     }
+
+    public static void initConfig() throws RetsServerException
+    {
+        File configFile = new File(Admin.getRexHome() +
+                                   "/webapp/WEB-INF/rex/rets-config.xml");
+        Admin.setConfigFile(configFile.getAbsolutePath());
+        Admin.setRetsConfig(
+            RetsConfig.initFromXmlFile(Admin.getConfigFile()));
+    }
+
+    public static void initDatabase() throws HibernateException
+    {
+        LOG.debug("Initializing Hibernate configuration");
+        RetsConfig retsConfig = Admin.getRetsConfig();
+        Configuration config = new Configuration()
+            .addJar("rex-hbm-xml.jar")
+            .setProperties(retsConfig.createHibernateProperties());
+        SessionFactory sessionFactory =
+            config.buildSessionFactory();
+        PasswordMethod.setDefaultMethod(PasswordMethod.DIGEST_A1,
+                                        PasswordMethod.RETS_REALM);
+        LOG.debug("Hibernate initialized");
+        Admin.setHibernateConfiguration(config);
+        RetsServer.setSessions(sessionFactory);
+    }
+
+    private static final Logger LOG =
+        Logger.getLogger(AdminUtils.class);
 }
