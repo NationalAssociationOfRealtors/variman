f9705511c3faf2c67b769f32c6b7fc5079bd3f93
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-05-10 22:17:48 +0000

    Figure out grid bag layout

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/ConfigurationPanel.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/ConfigurationPanel.java
@@ -15,7 +15,10 @@ public class ConfigurationPanel extends JPanel
 {
     public ConfigurationPanel()
     {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+
+        setLayout(new BorderLayout());
         JPanel label = new JPanel();
         label.setLayout(new BoxLayout(label, BoxLayout.X_AXIS));
         JLabel tmp = new JLabel("RETS");
@@ -23,23 +26,74 @@ public class ConfigurationPanel extends JPanel
         label.add(Box.createRigidArea(new Dimension(5, 0)));
         JPanel horizontalRule = new JPanel();
         horizontalRule.setBorder(
-            BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK));
+            BorderFactory.createMatteBorder(1, 0, 1, 0, Color.BLACK));
         horizontalRule.setLayout(new BoxLayout(horizontalRule, BoxLayout.Y_AXIS));
         horizontalRule.add(Box.createVerticalStrut(0));
         label.add(horizontalRule);
 
-        add(label);
-        Box labels = Box.createVerticalBox();
-        Box values = Box.createVerticalBox();
-        labels.add(new JLabel("Listening Port:"));
-        values.add(new JTextField(10));
-        labels.add(new JLabel("Metadata Directory:"));
-        values.add(new JTextField("c:\\blah"));
-        Box retsConfig = Box.createHorizontalBox();
-        retsConfig.add(labels);
-        retsConfig.add(values);
-        add(retsConfig);
+        panel2.add(label);
 
-        add(Box.createVerticalGlue());
+        JPanel retsConfig = new JPanel();
+//        retsConfig.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+        retsConfig.setLayout(gridbag);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weighty = 0.0;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(5, 5, 5, 5);
+
+        JLabel jLabel = new JLabel("Listening Port:");
+//        jLabel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 0.0;
+//        gridbag.setConstraints(jLabel, c);
+        retsConfig.add(jLabel, c);
+
+        JTextField jTextField = new JTextField(10);
+        c.gridx = 1;
+        c.gridy = 0;
+        c.weightx = 1.0;
+//        gridbag.setConstraints(jTextField, c);
+        retsConfig.add(jTextField, c);
+
+        jLabel = new JLabel("Metadatadatadata Directory:");
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 0.0;
+//        gridbag.setConstraints(jLabel, c);
+        retsConfig.add(jLabel, c);
+
+        jTextField = new JTextField("c:\\blah");
+        c.gridx = 1;
+        c.gridy = 1;
+        c.weightx = 1.0;
+//        gridbag.setConstraints(jTextField, c);
+        retsConfig.add(jTextField, c);
+
+        panel2.add(retsConfig);
+
+        panel2.add(new HeaderPanel("Database"));
+
+        TextValuePanel tvp = new TextValuePanel();
+        tvp.addRow("Some Text:", new JTextField());
+        tvp.addRow("Blah:", new JTextField());
+        tvp.setBorder(BorderFactory.createEmptyBorder(5, 30, 5, 5));
+        panel2.add(tvp);
+
+//        Box labels = Box.createVerticalBox();
+//        Box values = Box.createVerticalBox();
+//        labels.add(new JLabel("Listening Port:"));
+//        values.add(new JTextField(10));
+//        labels.add(new JLabel("Metadata Directory:"));
+//        values.add(new JTextField("c:\\blah"));
+//        Box retsConfig = Box.createHorizontalBox();
+//        retsConfig.add(labels);
+//        retsConfig.add(values);
+//        add(retsConfig);
+
+//        add(Box.createVerticalGlue());
+        add(panel2, BorderLayout.NORTH);
     }
 }
