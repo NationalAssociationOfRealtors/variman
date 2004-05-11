da5d6e9c3f7f09825f7ef2efaa8afbf11d72ca1c
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-05-11 20:09:29 +0000

    Add Mac Swing properties, init system properties

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/SwingMain.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/SwingMain.java
@@ -55,6 +55,11 @@ public class SwingMain
 
     public static void main(String[] args)
     {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name",
+                           "Rex Admin");
+        Admin.initSystemProperties();
+
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable()
