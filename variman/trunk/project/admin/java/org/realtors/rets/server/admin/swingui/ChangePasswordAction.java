cccaff123c87b6b1ef9a87abf40ecbdffa7bcd46
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-05-17 22:02:21 +0000

    Pull out inner dialog class to ChangePasswordDialog

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/ChangePasswordAction.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/ChangePasswordAction.java
@@ -1,17 +1,13 @@
 package org.realtors.rets.server.admin.swingui;
 
 import java.awt.event.ActionEvent;
-import java.awt.Frame;
-import java.awt.Component;
-import java.awt.Color;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 
-import org.realtors.rets.server.User;
-import org.realtors.rets.server.HibernateUtils;
-import org.apache.log4j.Logger;
 import net.sf.hibernate.HibernateException;
+import org.apache.log4j.Logger;
+import org.realtors.rets.server.HibernateUtils;
+import org.realtors.rets.server.User;
 
 public class ChangePasswordAction extends AbstractAction
 {
@@ -33,8 +29,7 @@ public class ChangePasswordAction extends AbstractAction
             }
 
             AdminFrame frame = SwingUtils.getAdminFrame();
-            GetPasswordDialog dialog = new GetPasswordDialog(
-                frame, "Enter new password for " + user.getName() + ":");
+            ChangePasswordDialog dialog = new ChangePasswordDialog(frame, user);
             dialog.show();
             if (dialog.getResponse() != JOptionPane.OK_OPTION)
             {
@@ -52,84 +47,6 @@ public class ChangePasswordAction extends AbstractAction
         }
     }
 
-    private class GetPasswordDialog extends JDialog
-    {
-        public GetPasswordDialog(Frame frame, String message)
-        {
-            super(frame);
-            setTitle("Change Password");
-            setModal(true);
-
-            JPanel content = new JPanel();
-            content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
-            JLabel label = new JLabel(message);
-            label.setAlignmentX(1.0f);
-            label.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
-            content.add(label);
-            mPasswordField = new JPasswordField();
-            content.add(mPasswordField);
-
-            Box buttonBox = Box.createHorizontalBox();
-            buttonBox.add(Box.createHorizontalGlue());
-            buttonBox.add(new JButton(new ChangePasswordButtonAction()));
-            buttonBox.add(Box.createHorizontalStrut(5));
-            buttonBox.add(new JButton(new CancelButtonAction()));
-            buttonBox.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
-            content.add(buttonBox);
-            content.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-            getContentPane().add(content);
-            pack();
-            setResizable(false);
-            SwingUtils.centerOnFrame(this, frame);
-            mResponse = JOptionPane.CANCEL_OPTION;
-        }
-
-        public String getPassword()
-        {
-            return mPassword;
-        }
-
-        public int getResponse()
-        {
-            return mResponse;
-        }
-
-        private class ChangePasswordButtonAction extends AbstractAction
-        {
-            public ChangePasswordButtonAction()
-            {
-                super("Change Password");
-            }
-
-            public void actionPerformed(ActionEvent event)
-            {
-                mResponse = JOptionPane.OK_OPTION;
-                mPassword = new String(mPasswordField.getPassword());
-                dispose();
-            }
-        }
-
-        private class CancelButtonAction extends AbstractAction
-        {
-            public CancelButtonAction()
-            {
-                super("Cancel");
-            }
-
-            public void actionPerformed(ActionEvent event)
-            {
-                mResponse = JOptionPane.CANCEL_OPTION;
-                dispose();
-            }
-
-        }
-
-        private int mResponse;
-        private JPasswordField mPasswordField;
-        private String mPassword;
-    }
 
     private static final Logger LOG =
         Logger.getLogger(ChangePasswordAction.class);
