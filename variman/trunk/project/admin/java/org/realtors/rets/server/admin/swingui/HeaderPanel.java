099de349dbc11ed3995eee2a596cf17ffa64fa89
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-05-18 20:39:00 +0000

    Use JSeparator instead of the border hack

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/HeaderPanel.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/HeaderPanel.java
@@ -1,15 +1,9 @@
 package org.realtors.rets.server.admin.swingui;
 
-import java.awt.*;
+import java.awt.Dimension;
+
 import javax.swing.*;
 
-/**
- * Created by IntelliJ IDEA.
- * User: dave
- * Date: May 10, 2004
- * Time: 11:59:22 AM
- * To change this template use File | Settings | File Templates.
- */
 public class HeaderPanel extends JPanel
 {
     public HeaderPanel(String title)
@@ -20,16 +14,12 @@ public class HeaderPanel extends JPanel
         add(Box.createRigidArea(new Dimension(5, 0)));
 
         JPanel horizontalRule = new JPanel();
-        horizontalRule.setBorder(
-            BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK));
-        // This trick creates a zero height panel.  The resulting output is
-        // just the top and botom borders butt right up against each other
-        // forming a solid horizontal line.
         horizontalRule.setLayout(new BoxLayout(horizontalRule,
                                                BoxLayout.Y_AXIS));
-        horizontalRule.add(Box.createVerticalStrut(0));
-
+        horizontalRule.add(Box.createVerticalGlue());
+        horizontalRule.add(new JSeparator());
         add(horizontalRule);
+
         setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     }
 }
