3d8709bbe55243072c7265cc29487c9d239cb4a3
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-07-29 19:40:01 +0000

    Considerably beefed up dialog

--- variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/RuleAddDialog.java
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/RuleAddDialog.java
@@ -1,8 +1,13 @@
 package org.realtors.rets.server.admin.swingui;
 
 import java.awt.event.ActionEvent;
+import java.util.Arrays;
 
 import javax.swing.*;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.realtors.rets.server.config.RuleDescription;
 
 public class RuleAddDialog extends JDialog
 {
@@ -11,7 +16,7 @@ public class RuleAddDialog extends JDialog
         super(SwingUtils.getAdminFrame());
         setModal(true);
         setTitle("Add Rule");
-        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         JPanel content = new JPanel();
         content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
@@ -23,11 +28,11 @@ public class RuleAddDialog extends JDialog
         tvp.addRow("Resource Name:", mResourceName);
         mClassName = new JTextField(TEXT_WIDTH);
         tvp.addRow("Class Name:", mClassName);
-        JTextArea textArea = new JTextArea();
-        textArea.setRows(5);
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        JScrollPane scrollPane = new JScrollPane(textArea);
+        mFields = new JTextArea();
+        mFields.setRows(5);
+        mFields.setLineWrap(true);
+        mFields.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(mFields);
         scrollPane.setVerticalScrollBarPolicy(
             JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         tvp.addRow("Fields:", scrollPane);
@@ -55,6 +60,37 @@ public class RuleAddDialog extends JDialog
         return mResponse;
     }
 
+    public RuleDescription.Type getType()
+    {
+        if (mRuleType.getSelectedIndex() == 0)
+        {
+            return RuleDescription.INCLUDE;
+        }
+        else
+        {
+            return RuleDescription.EXCLUDE;
+        }
+    }
+
+    public void setRuleDescription(RuleDescription ruleDescription)
+    {
+        if (mRuleType.getSelectedIndex() == 0)
+        {
+            ruleDescription.setType(RuleDescription.INCLUDE);
+        }
+        else
+        {
+            ruleDescription.setType(RuleDescription.EXCLUDE);
+        }
+
+        ruleDescription.setResource(mResourceName.getText());
+        ruleDescription.setRetsClass(mClassName.getText());
+
+        String[] systemNames = StringUtils.split(mFields.getText());
+        ruleDescription.setSystemNames(Arrays.asList(systemNames));
+    }
+
+
     private class AddRuleButtonAction extends AbstractAction
     {
         public AddRuleButtonAction()
@@ -64,8 +100,38 @@ public class RuleAddDialog extends JDialog
 
         public void actionPerformed(ActionEvent event)
         {
+            if (mResourceName.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(
+                    RuleAddDialog.this,
+                    "Please enter a resource name.",
+                    "Blank Resource Name",
+                    JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+
+            if (mClassName.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(
+                    RuleAddDialog.this,
+                    "Please enter a class name.",
+                    "Blank Class Name",
+                    JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+
+            if (mFields.getText().equals(""))
+            {
+                JOptionPane.showMessageDialog(
+                    RuleAddDialog.this,
+                    "Please enter system field names.",
+                    "Blank System Fields Name",
+                    JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+
             mResponse = JOptionPane.OK_OPTION;
-            setVisible(false);
+            dispose();
         }
     }
 
@@ -79,14 +145,14 @@ public class RuleAddDialog extends JDialog
         public void actionPerformed(ActionEvent event)
         {
             mResponse = JOptionPane.CANCEL_OPTION;
-            setVisible(false);
+            dispose();
         }
-
     }
 
     private static final int TEXT_WIDTH = 20;
     private JComboBox mRuleType;
     private JTextField mResourceName;
     private JTextField mClassName;
+    private JTextArea mFields;
     private int mResponse;
 }
