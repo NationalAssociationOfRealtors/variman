88da1641f05e2f923c0ac533f8d1676fd6616efc
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2004-08-02 21:11:09 +0000

    Create group rules, if none exist

--- variman/trunk/project/hibernate/java/org/realtors/rets/server/config/SecurityConstraints.java
+++ variman/trunk/project/hibernate/java/org/realtors/rets/server/config/SecurityConstraints.java
@@ -9,14 +9,30 @@ public class SecurityConstraints
         mGroupRules = constraints;
     }
 
-    public List getAllConstraints()
+    public List getAllGroupRules()
     {
         return mGroupRules;
     }
 
-    public GroupRules getRulesAt(int index)
+    public GroupRules getGroupRulesAt(int index)
     {
         return (GroupRules) mGroupRules.get(index);
+    }
+
+    public GroupRules getRulesForGroup(String groupName)
+    {
+        for (int i = 0; i < mGroupRules.size(); i++)
+        {
+            GroupRules groupRules = (GroupRules) mGroupRules.get(i);
+            if (groupRules.getGroupName().equals(groupName))
+            {
+                return groupRules;
+            }
+        }
+
+        GroupRules groupRules = new GroupRules(groupName);
+        mGroupRules.add(groupRules);
+        return groupRules;
     }
 
     private List mGroupRules;
