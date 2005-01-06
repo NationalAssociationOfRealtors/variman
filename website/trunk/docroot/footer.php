353b6329a3074bc1e664335442338f797ee94b68
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2005-01-06 17:40:27 +0000

    Update copyright

--- website/trunk/docroot/footer.php
+++ website/trunk/docroot/footer.php
@@ -1,6 +1,6 @@
 </div class="content">
 <div class="footer">
-    Copyright &copy; 2004 <a
+    Copyright &copy; 2004-2005 <a
     href="http://www.crt.realtors.org/">Center for REALTOR &reg;
     Technology</a>
   </div>
