4ccfd5f25fdf9b48809f8c29f3c38925a43b741c
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2005-01-21 21:15:08 +0000

    Add whole number field based on Sun's example implementation

---
+++ variman/trunk/project/admin/java/org/realtors/rets/server/admin/swingui/EditGroupAction.java
@@ -0,0 +1,62 @@
+package org.realtors.rets.server.admin.swingui;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+
+import org.apache.log4j.Logger;
+
+import org.realtors.rets.server.Group;
+import org.realtors.rets.server.HibernateUtils;
+import org.realtors.rets.server.admin.Admin;
+import org.realtors.rets.server.config.GroupRules;
+import net.sf.hibernate.HibernateException;
+
+public class EditGroupAction extends AbstractAction
+{
+    public EditGroupAction(GroupsPanel groupsPanel)
+    {
+        super("Edit Group...");
+        mGroupsPanel = groupsPanel;
+    }
+
+    public void actionPerformed(ActionEvent event)
+    {
+        try
+        {
+            Group group = mGroupsPanel.getSelectedGroup();
+            GroupRules rules = mGroupsPanel.getGroupRules();
+            if (group == null)
+            {
+                LOG.warn("Edit null group");
+            }
+
+            AdminFrame frame = SwingUtils.getAdminFrame();
+            EditGroupDialog dialog = new EditGroupDialog(frame, group, rules);
+            dialog.show();
+            if (dialog.getResponse() != JOptionPane.OK_OPTION)
+            {
+                return;
+            }
+
+            group.setDescription(dialog.getDescription());
+            HibernateUtils.update(group);
+            // Update this after hibernate, just in case of DB failure
+            rules.setRecordLimit(dialog.getRecordLimit());
+            frame.setStatusText("Group " + group.getName() + " changed");
+            frame.refreshGroups();
+            // Group rules are stored in RetsConfig
+            Admin.setRetsConfigChanged(true);
+            LOG.debug("Chagned group: " + group + ", record limit: " +
+                      dialog.getRecordLimit());
+        }
+        catch (HibernateException e)
+        {
+            LOG.error("Caught", e);
+        }
+    }
+
+    private static final Logger LOG =
+        Logger.getLogger(EditGroupAction.class);
+    private GroupsPanel mGroupsPanel;
+}
