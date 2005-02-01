908d3375e848c93e128d46bf9ac611dd1c8dbe21
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2005-02-01 16:14:30 +0000

    Test between, leass than, and greater than for temporal types (date, time, date time)

--- variman/trunk/project/hibernate/test/java/org/realtors/rets/server/dmql/AbstractDmqlCompilerTest.java
+++ variman/trunk/project/hibernate/test/java/org/realtors/rets/server/dmql/AbstractDmqlCompilerTest.java
@@ -28,7 +28,7 @@ public abstract class AbstractDmqlCompilerTest extends TestCase
         mMetadata.addNumeric("LP");
         mMetadata.addString("STR");
         mMetadata.addString("STYLE");
-        mMetadata.addString("LDATE");
+        mMetadata.addTemporal("LDATE");
         mMetadata.addString("REM");
         mMetadata.addString("OWNER");
     }
@@ -59,29 +59,53 @@ public abstract class AbstractDmqlCompilerTest extends TestCase
         }
     }
 
-    public void testBetweenPeriods() throws ANTLRException
+    public void testBetweenDates() throws ANTLRException
     {
         SqlConverter sql = parse("(LDATE=1970-01-01-1980-01-01)");
-        StringSqlConverter left = new StringSqlConverter("1970-01-01");
-        StringSqlConverter right = new StringSqlConverter("1980-01-01");
+        SqlConverter left = new DateSqlConverter("1970-01-01");
+        SqlConverter right = new DateSqlConverter("1980-01-01");
         BetweenClause between = new BetweenClause("r_LDATE", left, right);
         OrClause or = new OrClause();
         or.add(between);
         assertEquals(or, sql);
 
         sql = parse("(LDATE=1990-01-01-TODAY)");
-        left = new StringSqlConverter("1990-01-01");
-        right = new StringSqlConverter("TODAY");
+        left = new DateSqlConverter("1990-01-01");
+        right = new DateSqlConverter();
         between = new BetweenClause("r_LDATE", left, right);
         or = new OrClause();
         or.add(between);
         assertEquals(or, sql);
+    }
+
+    public void testBetweenDateTimes() throws ANTLRException
+    {
+        SqlConverter sql = parse(
+            "(LDATE=1990-01-01T05:06:07-1991-01-01T05:06:07.000)");
+        // The milliseconds are deliberately different as an additional test
+        SqlConverter left = new DateTimeSqlConverter("1990-01-01T05:06:07.000");
+        SqlConverter right = new DateTimeSqlConverter("1991-01-01T05:06:07");
+        BetweenClause between = new BetweenClause("r_LDATE", left, right);
+        OrClause or = new OrClause();
+        or.add(between);
+        assertEquals(or, sql);
 
         sql = parse("(LDATE=1990-01-01T05:06:07.000-NOW)");
-        left = new StringSqlConverter("1990-01-01T05:06:07.000");
-        right = new StringSqlConverter("NOW");
+        left = new DateTimeSqlConverter("1990-01-01T05:06:07.000");
+        right = new DateTimeSqlConverter();
         between = new BetweenClause("r_LDATE", left, right);
         or = new OrClause();
+        or.add(between);
+        assertEquals(or, sql);
+    }
+
+    public void testBetweenTimes() throws ANTLRException
+    {
+        SqlConverter sql = parse("(LDATE=05:06:07-13:57:02.468)");
+        SqlConverter left = new TimeSqlConverter("05:06:07");
+        SqlConverter right = new TimeSqlConverter("13:57:02.468");
+        BetweenClause between = new BetweenClause("r_LDATE", left, right);
+        OrClause or = new OrClause();
         or.add(between);
         assertEquals(or, sql);
     }
@@ -112,11 +136,48 @@ public abstract class AbstractDmqlCompilerTest extends TestCase
         assertEquals(or, sql);
     }
 
-    public void testLessThanPeriod() throws ANTLRException
+    public void testLessThanDate() throws ANTLRException
     {
-        parse("(OWNER=1970-01-01-)");
-        parse("(OWNER=TODAY-)");
-        parse("(OWNER=NOW-)");
+        SqlConverter sql = parse("(LDATE=1970-01-01-)");
+        LessThanClause lessThan =
+            new LessThanClause("r_LDATE", new DateSqlConverter("1970-01-01"));
+        OrClause or = new OrClause();
+        or.add(lessThan);
+        assertEquals(or, sql);
+
+        sql = parse("(LDATE=TODAY-)");
+        lessThan =
+            new LessThanClause("r_LDATE", new DateSqlConverter());
+        or = new OrClause();
+        or.add(lessThan);
+        assertEquals(or, sql);
+    }
+
+    public void testLessThanDateTime() throws ANTLRException
+    {
+        SqlConverter sql = parse("(LDATE=1990-01-01T05:06:07-)");
+        LessThanClause lessThan =
+            new LessThanClause(
+                "r_LDATE", new DateTimeSqlConverter("1990-01-01T05:06:07"));
+        OrClause or = new OrClause();
+        or.add(lessThan);
+        assertEquals(or, sql);
+
+        sql = parse("(LDATE=NOW-)");
+        lessThan = new LessThanClause("r_LDATE", new DateTimeSqlConverter());
+        or = new OrClause();
+        or.add(lessThan);
+        assertEquals(or, sql);
+    }
+
+    public void testLessThanTime() throws ANTLRException
+    {
+        SqlConverter sql = parse("(LDATE=13:57:02.468-)");
+        LessThanClause lessThan =
+            new LessThanClause("r_LDATE", new TimeSqlConverter("13:57:02.468"));
+        OrClause or = new OrClause();
+        or.add(lessThan);
+        assertEquals(or, sql);
     }
 
     public void testLessThanString() throws ANTLRException
@@ -134,11 +195,51 @@ public abstract class AbstractDmqlCompilerTest extends TestCase
         assertEquals(or, sql);
     }
 
-    public void testGreaterThanPeriod() throws ANTLRException
+    public void testGreaterThanDate() throws ANTLRException
     {
-        parse("(OWNER=1970-01-01+)");
-        parse("(OWNER=TODAY+)");
-        parse("(OWNER=NOW+)");
+        SqlConverter sql = parse("(LDATE=1970-01-01+)");
+        GreaterThanClause greaterThan =
+            new GreaterThanClause("r_LDATE",
+                                  new DateSqlConverter("1970-01-01"));
+        OrClause or = new OrClause();
+        or.add(greaterThan);
+        assertEquals(or, sql);
+
+        sql = parse("(LDATE=TODAY+)");
+        greaterThan =
+            new GreaterThanClause("r_LDATE", new DateSqlConverter());
+        or = new OrClause();
+        or.add(greaterThan);
+        assertEquals(or, sql);
+    }
+
+    public void testGreaterThanDateTime() throws ANTLRException
+    {
+        SqlConverter sql = parse("(LDATE=1990-01-01T05:06:07+)");
+        GreaterThanClause greaterThan =
+            new GreaterThanClause(
+                "r_LDATE", new DateTimeSqlConverter("1990-01-01T05:06:07"));
+        OrClause or = new OrClause();
+        or.add(greaterThan);
+        assertEquals(or, sql);
+
+        sql = parse("(LDATE=NOW+)");
+        greaterThan =
+            new GreaterThanClause("r_LDATE", new DateTimeSqlConverter());
+        or = new OrClause();
+        or.add(greaterThan);
+        assertEquals(or, sql);
+    }
+
+    public void testGreaterThanTime() throws ANTLRException
+    {
+        SqlConverter sql = parse("(LDATE=13:57:02.468+)");
+        GreaterThanClause greaterThan =
+            new GreaterThanClause(
+                "r_LDATE", new TimeSqlConverter("13:57:02.468"));
+        OrClause or = new OrClause();
+        or.add(greaterThan);
+        assertEquals(or, sql);
     }
 
     public void testGreaterThanString() throws ANTLRException
