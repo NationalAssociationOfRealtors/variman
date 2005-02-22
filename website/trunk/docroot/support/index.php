b7dd375f0da8ece4c7dc57995535f4f107604a1d
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2005-02-22 16:28:55 +0000

    Add email contact to info@crt

--- website/trunk/docroot/support/index.php
+++ website/trunk/docroot/support/index.php
@@ -7,5 +7,10 @@
       Please join the <a
         href="http://www.crt.realtors.org/mailman/listinfo/variman-users">variman-users mailing list</a>, and send questions there.
     </p>
+    <p>
+    For more information about the Center for REALTOR &reg; Technology
+    or Variman, please send email to <a
+    href="mailto:info@crt.realtors.org">info@crt.realtors.org</a>.
+    </p>
 
     <?php include("../footer.php"); ?>
