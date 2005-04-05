65b7ffdf14f3560ade5d4f042d310707653fb8ea
dribin <dribin@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2005-04-05 17:36:52 +0000

    Add tests with non-existant data

--- variman/trunk/project/hibernate/test/java/org/realtors/rets/server/protocol/ResidentialPropertyFormatterTest.java
+++ variman/trunk/project/hibernate/test/java/org/realtors/rets/server/protocol/ResidentialPropertyFormatterTest.java
@@ -55,6 +55,86 @@ public class ResidentialPropertyFormatterTest extends LinesEqualTestCase
         );
     }
 
+    public void testNoUnits() throws RetsServerException
+    {
+        String[] columns = new String[] {"r_STNAME", "r_ZIP_CODE"};
+        ResidentialPropertyFormatter formatter =
+            new ResidentialPropertyFormatter();
+        MockResultSet results = new MockResultSet();
+        results.setColumns(COLUMNS);
+        results.addRow(new String[] {"Main St.", "12345"});
+        SimpleDmqlMetadata metadata = new SimpleDmqlMetadata();
+        metadata.addString("StreetName", "r_STNAME");
+        metadata.addString("PostalCode", "r_ZIP_CODE");
+
+        StringWriter formatted = new StringWriter();
+        SearchFormatterContext context =
+            new SearchFormatterContext(new PrintWriter(formatted), results,
+                                       Arrays.asList(COLUMNS), metadata);
+        formatter.formatResults(context);
+        assertLinesEqual(
+            "<REData>\n" +
+            "<REProperties>\n" +
+            "<ResidentialProperty>\n" +
+            "<Listing>\n" +
+            "<StreetAddress>\n" +
+            "<StreetNumber/>\n" +
+            "<StreetName>Main St.</StreetName>\n" +
+            "<PostalCode>12345</PostalCode>\n" +
+            "</StreetAddress>\n" +
+            "</Listing>\n" +
+            "<LivingArea>\n" +
+            "<Area></Area>\n" +
+            "</LivingArea>\n" +
+            "</ResidentialProperty>\n" +
+            "</REProperties>\n" +
+            "</REData>\n",
+            formatted.toString()
+        );
+    }
+
+    public void testNoStreetNameColumn() throws RetsServerException
+    {
+        String[] columns = new String[] {"r_ZIP_CODE", "r_SQFT"};
+        ResidentialPropertyFormatter formatter =
+            new ResidentialPropertyFormatter();
+        MockResultSet results = new MockResultSet();
+        results.setColumns(columns);
+        results.addRow(new String[] {"12345", "1000"});
+        SimpleDmqlMetadata metadata = new SimpleDmqlMetadata();
+        metadata.addString("StreetName", "r_STNAME");
+        metadata.addString("PostalCode", "r_ZIP_CODE");
+        metadata.addString("LivingArea", "r_SQFT");
+        Table table = new Table(1);
+        table.setUnits(UnitEnum.SQFT);
+        metadata.addTable("LivingArea", table);
+
+        StringWriter formatted = new StringWriter();
+        SearchFormatterContext context =
+            new SearchFormatterContext(new PrintWriter(formatted), results,
+                                       Arrays.asList(columns), metadata);
+        formatter.formatResults(context);
+        assertLinesEqual(
+            "<REData>\n" +
+            "<REProperties>\n" +
+            "<ResidentialProperty>\n" +
+            "<Listing>\n" +
+            "<StreetAddress>\n" +
+            "<StreetNumber/>\n" +
+            "<StreetName></StreetName>\n" +
+            "<PostalCode>12345</PostalCode>\n" +
+            "</StreetAddress>\n" +
+            "</Listing>\n" +
+            "<LivingArea>\n" +
+            "<Area Units=\"SqFeet\">1000</Area>\n" +
+            "</LivingArea>\n" +
+            "</ResidentialProperty>\n" +
+            "</REProperties>\n" +
+            "</REData>\n",
+            formatted.toString()
+        );
+    }
+
     public static final String[] COLUMNS =
         new String[] {"r_STNAME", "r_ZIP_CODE", "r_SQFT"};
 }
