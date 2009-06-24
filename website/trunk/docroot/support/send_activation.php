7d8630b6d2ee8ec4b23ffa4ac002d1e0f1df53d3
atillman <atillman@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2009-06-24 17:13:38 +0000

    Clean up the properties display

--- website/trunk/docroot/support/send_activation.php
+++ website/trunk/docroot/support/send_activation.php
@@ -6,7 +6,7 @@
 <?php
  $email = $_POST['email'];
  $hash = md5($_POST['host'].$email);
- $properties = '#This is your activation file.  Put it in $VARIMAN_HOME/webapps/WEB-INF\n';
+ $properties = "#This is your activation file.  Put it in VARIMAN_HOME/webapps/WEB-INF\n";
  $properties = $properties."variman.activation.email=$email\n";
  $properties = $properties."variman.activation.code=$hash" ;
 
@@ -16,4 +16,7 @@
 <p>The activation file has been sent to you via email at the address '<?php echo $email?>'.  This email will have the file "activation.properties" attached to it.  
 Put it in your Variman install in the directory "$VARIMAN_HOME/webapp/WEB-INF".  </p>
 
-HASH: <?php echo $properties?>
+Properties:<br/>
+<pre>
+<?php echo $properties?>
+</pre>
