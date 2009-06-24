e7fe4d743ac6a873a8e244008910cf40a6ab21c7
atillman <atillman@d8515cf8-2b33-88e6-75ea-425dd0745e11>
2009-06-24 21:43:20 +0000

    It now sends email.

--- website/trunk/docroot/support/send_activation.php
+++ website/trunk/docroot/support/send_activation.php
@@ -6,17 +6,39 @@
 <?php
  $email = $_POST['email'];
  $hash = md5($_POST['host'].$email);
- $properties = "#This is your activation file.  Put it in VARIMAN_HOME/webapps/WEB-INF\n";
- $properties = $properties."variman.activation.email=$email\n";
- $properties = $properties."variman.activation.code=$hash" ;
+ $properties = "#This is your activation file.  Put it in \$VARIMAN_HOME/webapps/WEB-INF\n";
+ $properties .= "variman.activation.email=$email\n";
+ $properties .= "variman.activation.code=$hash" ;
+
+$message = "Your variman activation file is attached as the file activation.properties.  Please put this file in \$VARIMAN_HOME/webapps/WEB-INF\n";
+
+$results = send_activation($email, "Your Variman Activation File", $message, $properties);
+
+function send_activation($to,$subject,$body,$attachment){
+
+ $semi_rand = md5( time() );
+ $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+ $headers = "From: Variman Activation <variman.activation@crt.realtors.org>\n";
+ $headers .= "Reply-To: CRT Information <info@crt.realtors.org>\n";
+ $headers .= "Content-Type: multipart/mixed; boundary=\"{$mime_boundary}\"";  // space *needs* to be there
+
+$message = "This is a multi-part message in MIME format.\n\n".
+  "--{$mime_boundary}\n".
+  "Content-Type: text/plain; charset=ISO-8859-1;\n".
+  "Content-Transfer-Encoding: 7bit\n\n".
+  $body."\n\n".
+  "--{$mime_boundary}\n".
+  "Content-Type: text/plain; charset=ISO-8859-1; name=\"activation.properties\"\n".
+  "Content-Transfer-Encoding: 7bit\n".
+  "Content-Disposition: attachment; filename=\"activation.properties\"\n\n".
+  $attachment."\n\n".
+  "--{$mime_boundary}--";
+
+ return mail($to,$subject,$message,$headers);
+}
 
 ?>
 
 <h3>Variman Activation</h3>
 <p>The activation file has been sent to you via email at the address '<?php echo $email?>'.  This email will have the file "activation.properties" attached to it.  
 Put it in your Variman install in the directory "$VARIMAN_HOME/webapp/WEB-INF".  </p>
-
-Properties:<br/>
-<pre>
-<?php echo $properties?>
-</pre>
